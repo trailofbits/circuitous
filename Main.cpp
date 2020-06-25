2099a30555cbd83059c43c2abeda042901ed4e8c
Claudia Richoux <claudia.richoux@trailofbits.com>
2020-06-24 22:48:46 -0400

    making circuit output to python more useful for parsing

--- Main.cpp
+++ Main.cpp
@@ -118,45 +118,48 @@ int main(int argc, char *argv[]) {
 
     std::ofstream os(FLAGS_python_out);
     os << "def circuit():\n"
-       << "  v" << std::hex << reinterpret_cast<uintptr_t>(circuit.get()) << " = []\n";
+       << "  operations = {}\n"
+       << "  result = 'v" << std::hex << reinterpret_cast<uintptr_t>(circuit.get()) << "'\n"
+       << "  operations[result] = []\n";
 
     circuit->ForEachOperation([&] (circuitous::Operation *op) {
-      os << "  v" << reinterpret_cast<uintptr_t>(op) << " = []\n";
+      os << "  operations['v" << reinterpret_cast<uintptr_t>(op) << "'] = []\n";
     });
 
     auto do_op = [&] (circuitous::Operation *op) {
       const auto id = reinterpret_cast<uintptr_t>(op);
-      os << "  v" << std::hex << id << ".append(\"" << op->Name() << "\")\n"
-         << "  v" << std::hex << id << ".append("
+      os << "  operations['v" << std::hex << id << "'].append(\"" << op->Name() << "\")\n"
+         << "  operations['v" << std::hex << id << "'].append("
          << std::dec << static_cast<unsigned>(op->op_code) << ")\n"
-         << "  v" << std::hex << id << ".append("
+         << "  operations['v" << std::hex << id << "'].append("
          << std::dec << op->size << ")\n";
 
       if (auto llvm_op = dynamic_cast<circuitous::LLVMOperation *>(op); llvm_op) {
-        os << "  v" << std::hex << id << ".append("
+        os << "  operations['v" << std::hex << id << "'].append("
            << std::dec << llvm_op->llvm_op_code << ")\n"
-           << "  v" << std::hex << id << ".append("
+           << "  operations['v" << std::hex << id << "'].append("
            << std::dec << llvm_op->llvm_predicate << ")\n";
 
       } else if (auto extract_op = dynamic_cast<circuitous::Extract *>(op);
                  extract_op) {
-        os << "  v" << std::hex << id << ".append("
+        os << "  operations['v" << std::hex << id << "'].append("
            << std::dec << extract_op->high_hit_exc << ")\n"
-           << "  v" << std::hex << id << ".append("
+           << "  operations['v" << std::hex << id << "'].append("
            << std::dec << extract_op->low_bit_inc << ")\n";
       }
 
       for (auto sub_op : op->operands) {
-        os << "  v" << std::hex << id << ".append(v"
+        os << "  operations['v" << std::hex << id << "'].append('v"
            << reinterpret_cast<uintptr_t>(sub_op)
-           << std::dec << ")\n";
+           << std::dec << "')\n";
       }
     };
 
     circuit->ForEachOperation(do_op);
     do_op(circuit.get());
 
-    os << "  return v" << std::hex << reinterpret_cast<uintptr_t>(circuit.get())
+    //os << "  return v" << std::hex << reinterpret_cast<uintptr_t>(circuit.get())
+    os << "  return operations"
        << "\n\n" << std::dec;
   }
 
