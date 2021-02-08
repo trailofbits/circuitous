677b0184585a0033fd95ef43f4fe403bd0f76672
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-02-08 17:48:04 +0100

    util: Gut LLVMUtil as it would probably only clutter the code.

--- include/circuitous/Util/LLVMUtil.hpp
+++ include/circuitous/Util/LLVMUtil.hpp
@@ -7,53 +7,19 @@
 // TODO(lukas): Fill llvm headers
 #include <string>
 
-/* Idea is that we provide some basic wrappers that bind llvm::Module and
- * llvm::LLVMContext so we do not need to specify them everywhere (mostly
- * when creating types or values).
- */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wconversion"
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Module.h>
+#pragma clang diagnostic pop
 
 namespace circuitous {
-  std::string LLVMName(llvm::Value *val,
-                       const std::string &def = "(name not set)") {
+  static inline std::string LLVMName(llvm::Value *val,
+                                     const std::string &def = "(name not set)") {
     if (!val->hasName()) {
       return def;
     }
     return val->getName().str();
   }
-
-  template<typename Self_t, template<typename ...> class Derived>
-  struct Crtp {
-    Self_t &Self() { return static_cast<Self_t &>(*this); }
-    const Self_t &Self() const { return static_cast<Self_t &>(*this); }
-
-    auto ctx() { return Self()._ctx; }
-  };
-
-  template<typename Self_t>
-  struct LLVMTypesMixin : Crtp<Self_t, LLVMTypesMixin> {
-    using parent_t = Crtp<Self_t, LLVMTypesMixin>;
-    using parent_t::ctx;
-
-    using types_t = std::vector<llvm::Type *>;
-
-    auto Void() { return llvm::Type::getVoidTy(*ctx()); }
-    auto FnTy(llvm::Type *ret, const types_t &args, bool is_vararg=false) {
-        return llvm::FunctionType::get(ret, args, is_vararg);
-    }
-  };
-
-  struct WithModule {
-      llvm::Module *_module;
-      llvm::LLVMContext *_ctx;
-
-      WithModule(llvm::Module *module_)
-        : _module(module_), _ctx(&_module->getContext())
-      {}
-  };
-
-  struct LLVMTypes : LLVMTypesMixin<LLVMTypes>, WithModule {
-      using WithModule::WithModule;
-  };
-
-
 } // namespace circuitous
