e5a92a3676c1392f551befc9fc7bf0d6344b22be
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-02-24 16:05:04 +0100

    util: Add really simple instruction metadata wrappers.

--- include/circuitous/Util/LLVMUtil.hpp
+++ include/circuitous/Util/LLVMUtil.hpp
@@ -5,13 +5,17 @@
 #pragma once
 
 // TODO(lukas): Fill llvm headers
+#include <optional>
 #include <string>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wsign-conversion"
 #pragma clang diagnostic ignored "-Wconversion"
-#include <llvm/IR/Value.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Metadata.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Value.h>
 #pragma clang diagnostic pop
 
 namespace circuitous {
@@ -22,4 +26,27 @@ namespace circuitous {
     }
     return val->getName().str();
   }
+
+  template<typename T>
+  void AddMetadata(llvm::Instruction *inst, const std::string &kind, T value) {
+    CHECK(!inst->getMetadata(kind));
+    auto &ctx = inst->getContext();
+    llvm::IRBuilder<> ir(ctx);
+    auto node = llvm::MDNode::get(
+      ctx, {llvm::MDString::get(ctx, std::to_string(value))});
+    inst->setMetadata(kind, node);
+  }
+
+  // If you feel like you need to return different type just extend it.
+  static inline std::optional<uint64_t> GetMetadata(llvm::Instruction *inst,
+                                                    const std::string &kind) {
+    auto node = inst->getMetadata(kind);
+    if (!node) {
+      return {};
+    }
+    CHECK(node->getNumOperands() == 1);
+    auto op = llvm::dyn_cast<llvm::MDString>(node->getOperand(0));
+    return std::strtoull(op->getString().data(), nullptr, 10);
+  }
+
 } // namespace circuitous
