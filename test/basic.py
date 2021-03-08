b312f7c1edc93159a7fff42f43092efc437b9fa9
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-03-08 02:25:07 +0100

    test: Add test lifting greater number of instructions.

--- test/basic.py
+++ test/basic.py
@@ -225,6 +225,37 @@ test_ip = {
   .case(I = S().RDX(0x0).RIP(0x1000), E = S().RDX(0x12).RIP(0x1005), R = True)
 }
 
+test_all = {
+  ModelTest("adc, add, sub, sbb, shl, shr, xor, or, and, div, idiv").tags({"opts"})
+  .bytes(intel(["adc rax, 0x12", "add rcx, rax", "add rax, 0x45",
+                "sub rcx, 0x15", "sub rax, rcx", "sbb rax, 0x12",
+                "shr rax, 0x2", "shr rcx, 0x2", "shl rax, 0x2", "shl rcx, 0x2",
+                "xor rax, rax", "xor rax, rcx",
+                "or rax, rax","or rax, rcx",
+                "and rax, 0xfffff", "and rax, rcx",
+                "div rax", "idiv rcx", "idiv rax"]))
+  .DI(S().RAX(0x12).RCX(0x45).aflags(0))
+  .case(run_bytes = 0, R=True)
+  .case(run_bytes = 1, R=True)
+  .case(run_bytes = 2, R=True)
+  .case(run_bytes = 3, R=True)
+  .case(run_bytes = 4, R=True)
+  .case(run_bytes = 5, R=True)
+  .case(run_bytes = 6, R=True)
+  .case(run_bytes = 7, R=True)
+  .case(run_bytes = 8, R=True)
+  .case(run_bytes = 9, R=True)
+  .case(run_bytes = 10, R=True)
+  .case(run_bytes = 11, R=True)
+  .case(run_bytes = 12, R=True)
+  .case(run_bytes = 13, R=True)
+  .case(run_bytes = 14, R=True)
+  .case(run_bytes = 15, R=True)
+  .case(run_bytes = 16, DE = MS().aflags(0), R=True)
+  .case(run_bytes = 17, DE = MS().aflags(0), R=True)
+  .case(run_bytes = 18, DE = MS().aflags(0), R=True)
+}
+
 # TODO(lukas): Division by zero
 
 circuitous_tests = \
@@ -232,5 +263,6 @@ circuitous_tests = \
     test_shl, test_shr,
     test_xor, test_or, test_and,
     test_div, test_idiv,
-    test_ip
+    test_ip,
+    test_all
   ]
