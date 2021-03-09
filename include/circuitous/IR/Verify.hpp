abd19598702048f2d5bcd952888c8fa676a7cb42
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-03-09 21:44:23 +0100

    ir: Add some more verify checks based around hints.

--- include/circuitous/IR/Verify.hpp
+++ include/circuitous/IR/Verify.hpp
@@ -6,6 +6,8 @@
 
 #include <cstdint>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 
 #include <circuitous/IR/IR.h>
 
@@ -48,7 +50,7 @@ struct Verifier {
   }
 
 
-  bool VerifySelf(Operation *op) {
+  bool VerifyArity(Operation *op) {
     switch (op->op_code) {
       case Operation::kConstant:
       case Operation::kUndefined:
@@ -69,6 +71,7 @@ struct Verifier {
       case Operation::kPreservedCondition:
       case Operation::kCopyCondition:
       case Operation::kDecodeCondition:
+      case Operation::kHintCondition:
         return Exactly(2, op);
       case Operation::kConcat:
         return MoreThan(2, op);
@@ -88,11 +91,76 @@ struct Verifier {
   }
 
   bool Verify(Operation *op) {
-    status &= VerifySelf(op);
+    status &= VerifyArity(op);
     for (auto o : op->operands) {
       status &= Verify(o);
     }
     return status;
+  }
+
+  void VerifyHints(Circuit *circuit) {
+    status &= VerifyHintChecks(circuit);
+    status &= VerifyHintUsers(circuit);
+  }
+
+  // We try to check if there is not a HINT with more than one HINT_CHECK
+  // in the same context, since that is an error.
+  bool VerifyHintChecks(Circuit *circuit) {
+    bool out = true;
+    std::unordered_map<Operation *, std::unordered_set<VerifyInstruction *>> hint_to_ctxs;
+    std::unordered_map<Operation *, VerifyInstruction *> ctx;
+    for (auto verif : circuit->verifications) {
+      for (auto op : verif->operands) {
+        if (op->op_code == Operation::kHintCondition) {
+          ctx[op] = verif;
+
+          bool found_hint = false;
+          for (auto hint : op->operands) {
+            if (hint->op_code == Operation::kHint) {
+              if (found_hint) {
+                ss << "HINT_CHECK has at least two direct HINT operands!\n";
+                status = false;
+              }
+              found_hint = true;
+              if (hint_to_ctxs[hint].count(verif)) {
+                ss << "Hint is under two hint checks in the same context!\n";
+                status = false;
+              }
+            }
+          }
+          if (!found_hint) {
+            ss << "HINT_CHECK does not have HINT as direct operand!\n";
+            status = false;
+          }
+        }
+      }
+    }
+    return out;
+  }
+
+  using hint_users_t = std::unordered_map<Operation *, std::unordered_set<Operation *>>;
+  void CollectHintUsers(Operation *op, hint_users_t &collected ) {
+    for (auto child : op->operands) {
+      if (child->op_code == Operation::kHint) {
+        collected[child].insert(op);
+      } else {
+        CollectHintUsers(child, collected);
+      }
+    }
+  }
+
+  bool VerifyHintUsers(Operation *circuit) {
+    hint_users_t collected;
+    CollectHintUsers(circuit, collected);
+
+    bool out = true;
+    for (auto &[hint, users] : collected) {
+      if (users.size() <= 1) {
+        ss << "HINT has only one user. High possibility of error!\n";
+        out = false;
+      }
+    }
+    return out;
   }
 
 };
@@ -102,6 +170,7 @@ struct Verifier {
 static inline std::pair<bool, std::string> VerifyCircuit(Circuit *circuit) {
   Verifier verifier;
   verifier.Verify(circuit);
+  verifier.VerifyHints(circuit);
   return {verifier.status, verifier.Report()};
 }
 
