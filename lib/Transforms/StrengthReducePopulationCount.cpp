9863dc9db8ec75d17fb5dfd340bfe806e99ba576
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-03-10 19:54:13 +0100

    opt: Update to newer IR API.

--- lib/Transforms/StrengthReducePopulationCount.cpp
+++ lib/Transforms/StrengthReducePopulationCount.cpp
@@ -14,10 +14,10 @@ namespace circuitous {
 // Look for uses of population count that operates on a zero-extended value, and
 // change it to operate on the original value.
 bool StrengthReducePopulationCount(Circuit *circuit) {
-  circuit->popcounts.RemoveUnused();
-  const auto num_pop_counts = circuit->popcounts.Size();
+  circuit->Attr<PopulationCount>().RemoveUnused();
+  const auto num_pop_counts = circuit->Attr<PopulationCount>().Size();
   for (auto i = 0u; i < num_pop_counts; ++i) {
-    const auto inst = circuit->popcounts[i];
+    const auto inst = circuit->Attr<PopulationCount>()[i];
     const auto val = inst->operands[0];
 
     auto llvm_ir = dynamic_cast<LLVMOperation *>(val);
@@ -26,8 +26,8 @@ bool StrengthReducePopulationCount(Circuit *circuit) {
     }
 
     const auto real_val = val->operands[0];
-    auto new_popcount = circuit->popcounts.Create(real_val->size);
-    auto new_zext = circuit->llvm_insts.Create(
+    auto new_popcount = circuit->Create<PopulationCount>(real_val->size);
+    auto new_zext = circuit->Create<LLVMOperation>(
         llvm::Instruction::ZExt, LLVMOperation::kInvalidLLVMPredicate,
         inst->size);
 
@@ -36,7 +36,7 @@ bool StrengthReducePopulationCount(Circuit *circuit) {
     new_popcount->operands.AddUse(real_val);
   }
 
-  return 0u < circuit->popcounts.RemoveUnused();
+  return 0u < circuit->Attr<PopulationCount>().RemoveUnused();
 }
 
 }  // namespace circuitous
