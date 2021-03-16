8fd91252614730c4b8b413b4b85b9f8c98db05f3
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-03-16 14:04:32 +0100

    opt: Add really basic and simple pass manager.

--- include/circuitous/Transforms.h
+++ include/circuitous/Transforms.h
@@ -4,6 +4,18 @@
 
 #pragma once
 
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <circuitous/IR/Verify.hpp>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wconversion"
+#include <glog/logging.h>
+#pragma clang diagnostic pop
+
 namespace circuitous {
 
 class Circuit;
@@ -25,5 +37,70 @@ bool ExtractCommonTopologies(Circuit *circuit);
 // sufficient size to support all verifiers. In place of the individual hints,
 // the verifiers pull out slices of wide hint value with an EXTRACT.
 bool MergeHints(Circuit *circuit);
+
+bool MergeByBytes(Circuit *circuit);
+
+struct KnownPasses {
+  using pass_t = bool(*)(Circuit *);
+  using storage_t  = std::unordered_map<std::string, bool(*)(Circuit *)>;
+
+  static const inline storage_t passes = {
+    {"popcount2parity", ConvertPopCountToParity},
+    {"reducepopcount", StrengthReducePopulationCount},
+    {"extractcommon", ExtractCommonTopologies},
+    {"mergehints", MergeHints},
+    {"mergebybytes", MergeByBytes}
+  };
+
+  static pass_t Get(const std::string &name) {
+    auto it = passes.find(name);
+    CHECK(it != passes.end());
+    return it->second;
+  }
+
+  static void Run(Circuit *circuit, const std::string &name) {
+    Get(name)(circuit);
+  }
+};
+
+
+struct OptManager : KnownPasses {
+  using KnownPasses::Run;
+
+  std::unordered_set<pass_t> selected;
+
+  void AddPass(const std::string &name) {
+    selected.insert(Get(name));
+  }
+
+  std::vector<pass_t> Order() {
+    LOG(FATAL) << "Implement ordering!";
+  }
+
+  void Run(Circuit *circuit) {
+    for (auto pass : selected) {
+      pass(circuit);
+    }
+  }
+};
+
+template<typename Logger>
+struct DefensiveOptManger : OptManager {
+
+  void Run(Circuit *circuit, const std::string &name) {
+    Logger::log("Going to run transformation", name);
+    this->OptManager::Run(circuit, name);
+    Logger::log("Done. Running verify pass:");
+    const auto &[status, msg] = VerifyCircuit(circuit);
+    if (!status) {
+      Logger::fail("Verify failed");
+      Logger::fail(msg);
+      Logger::kill();
+    }
+    Logger::log("Circuit is okay.");
+  }
+};
+
+
 
 }  // namespace circuitous
