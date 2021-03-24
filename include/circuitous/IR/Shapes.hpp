4c258b757c381812c1e61dfd84dabc6aa0bb08c6
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-03-24 22:15:31 +0100

    ir: Update Shapes.hpp with newer utilities and reworked Collector.

--- include/circuitous/IR/Shapes.hpp
+++ include/circuitous/IR/Shapes.hpp
@@ -41,6 +41,23 @@ static inline bool IsLeaf(Operation *op) {
   }
 }
 
+template<typename T>
+bool Is(Operation *op) {
+  return op->op_code == T::kind;
+}
+
+// Return number of HINT users that
+static inline std::size_t HintUsers(Operation *hint) {
+  CHECK(Is<Hint>(hint));
+  std::size_t users = 0;
+  for (auto user : hint->users) {
+    if (!Is<HintCondition>(user)) {
+      ++users;
+    }
+  }
+  return users;
+}
+
 using operation_set_t = std::unordered_set<Operation *>;
 
 template<typename T>
@@ -76,18 +93,151 @@ struct SubtreeCollector {
   }
 };
 
-struct CtxCollector {
-  using ctxs_t = std::unordered_set<Operation *>;
-  using ctxs_map_t = std::unordered_map<Operation *, ctxs_t>;
 
-  ctxs_map_t op_to_ctxs;
+
+namespace print {
+  template<typename Derived>
+  struct Topology {
+    std::stringstream ss;
+    using hash_t = std::string;
+
+    Derived &Self() { return static_cast<Derived &>(*this); }
+
+    template<typename C>
+    std::string Hash(const C& ops) {
+      std::stringstream ss;
+      for (auto op : ops) {
+        ss << Hash(op);
+        ss << " | ";
+      }
+      return ss.str();
+    }
+
+    std::string Hash(Operation *op) {
+      return Self().Print(op, 0);
+    }
+
+    std::string Children(Operation *op, uint8_t depth) {
+      std::string out;
+      for (auto o : op->operands) {
+        out += Self().Print(o, depth + 1);
+        out += Self().separator;
+      }
+      return out;
+    }
+
+    std::string Print(Operation *op) {
+      return Self().Print(op, 0);
+    }
+
+    std::string Print(Operation *op, uint8_t depth) {
+      auto indent = Self().Indent(depth);
+      std::string out;
+      out += indent;
+      out += Self().Op(op);
+      out += "( ";
+      out += Self().Children(op, depth);
+      out += indent + ")";
+      return out;
+    }
+
+    std::string Get() { return ss.str(); }
+    std::string Indent(uint8_t) { return {}; }
+  };
+
+  template<typename Next>
+  struct WithCache : Next {
+    using parent_t = Next;
+    using hash_t = typename parent_t::hash_t;
+
+    using Next::Hash;
+
+    std::unordered_map<Operation *, hash_t > op_to_hash;
+
+    std::string Print(Operation *op, uint8_t depth) {
+      auto it = op_to_hash.find(op);
+      if (it != op_to_hash.end()) {
+        return it->second;
+      }
+      auto x = this->parent_t::Print(op, depth + 1);
+      op_to_hash[op] = x;
+      return x;
+    }
+  };
+
+  template<typename Next>
+  struct FullNames_ : Next {
+    static inline constexpr const char separator = ' ';
+    std::string Op(Operation *op) { return op->Name(); }
+  };
+
+  struct FullNames : FullNames_<WithCache<Topology<FullNames>>> {};
+
+  struct PrettyPrinter : FullNames {
+    std::string Indent(uint8_t depth) {
+      return std::string(depth * 2, ' ');
+    }
+  };
+
+} // namespace print
+
+
+namespace collect {
+  struct Ctxs {
+    using ctxs_t = std::unordered_set<Operation *>;
+    using ctxs_map_t = std::unordered_map<Operation *, ctxs_t>;
+
+    ctxs_map_t op_to_ctxs;
+
+    void Root(Operation *op) {
+      op_to_ctxs[op] = { op };
+    }
+
+    void Update(Operation *node, Operation *user) {
+      if (user) {
+        auto &ctxs = op_to_ctxs[node];
+        auto &user_ctxs = op_to_ctxs[user];
+        ctxs.insert(user_ctxs.begin(), user_ctxs.end());
+      }
+    }
+  };
+
+  struct Hashes : print::FullNames {
+    void Root(Operation *op) {
+      Hash(op);
+    }
+
+    void Update(Operation *node, Operation *user) {
+      CHECK(op_to_hash.count(node));
+    }
+  };
+
+
+  template< typename ...Ts >
+  struct UpTree {
+    std::unordered_set<Operation *> collected;
+
+    void Run(Operation *op) {
+      if (IsOneOf<Ts...>(op)) {
+        collected.insert(op);
+      }
+      for (auto o : op->users) {
+        Run(o);
+      }
+    }
+  };
+} // namespace collect
+
+template<typename ...Collectors>
+struct Collector : Collectors ... {
+  using self_t = Collector<Collectors...>;
 
   using entry_t = std::pair<Operation *, Operation *>;
   std::deque<entry_t> todo;
 
-  CtxCollector &Run(Circuit *circuit) {
+  self_t &Run(Circuit *circuit) {
     for (auto x : circuit->Attr<VerifyInstruction>()) {
-      op_to_ctxs[x] = {x};
+      (Collectors::Root(x), ...);
       todo.push_back({x, nullptr});
     }
 
@@ -100,122 +250,28 @@ struct CtxCollector {
   }
 
   void Update(Operation *node, Operation *user) {
-    if (user) {
-      auto &ctxs = op_to_ctxs[node];
-      auto &user_ctxs = op_to_ctxs[user];
-      ctxs.insert(user_ctxs.begin(), user_ctxs.end());
-    }
+    (Collectors::Update(node, user), ...);
     for (auto op : node->operands) {
       todo.emplace_back(op, node);
     }
   }
-
-  ctxs_map_t Get() { return op_to_ctxs; }
 };
 
-namespace cmp {
+using CtxCollector = Collector<collect::Ctxs>;
 
-struct StrictComparator {
-  template<typename T>
-  auto ForwardAs(Operation *lhs, Operation *rhs) {
-    return Compare(dynamic_cast<T *>(lhs), dynamic_cast<T *>(rhs));
-  }
-
-  bool Compare(Operation *lhs, Operation *rhs) {
-    return lhs->Equals(rhs);
-    if (lhs->op_code != rhs->op_code || lhs->operands.Size() != rhs->operands.Size()) {
-      return false;
-    }
-    switch (lhs->op_code) {
-      case Operation::kConstant:
-        return ForwardAs<Constant>(lhs, rhs);
-      case Operation::kInputRegister:
-        return ForwardAs<InputRegister>(lhs, rhs);
-      case Operation::kOutputRegister:
-        return ForwardAs<OutputRegister>(lhs, rhs);
-      case Operation::kLLVMOperation:
-        if (!ForwardAs<LLVMOperation>(lhs, rhs)) return false;
-      case Operation::kExtract:
-        if (!ForwardAs<Extract>(lhs, rhs)) return false;
-      case Operation::kHint:
-        if (!ForwardAs<Hint>(lhs, rhs)) return false;
-      default:
-        break;
-    }
-    for (auto i = 0ull; i < lhs->operands.Size(); ++i) {
-      if (!Compare(lhs->operands[i], rhs->operands[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-
-  bool Compare(Constant *a, Constant *b) { return a->bits == b->bits; }
-  bool Compare(InputRegister *a, InputRegister *b) { return a->reg_name == b->reg_name; }
-  bool Compare(LLVMOperation *a, LLVMOperation *b) { return false; }
-  bool Compare(Extract *a, Extract *b) { return false; }
-};
-
-static inline bool StrictStructural(Operation *rhs, Operation *lhs) {
-  return false;
+static inline Operation *GetContext(Operation *op) {
+  collect::UpTree<VerifyInstruction> collector;
+  collector.Run(op);
+  auto &ctxs = collector.collected;
+  CHECK(ctxs.size() == 1);
+  return *(ctxs.begin());
 }
 
-} // namespace cmp
-
-namespace print {
-
-template<typename Derived>
-struct Topology {
-  std::stringstream ss;
-
-  Derived &Self() { return static_cast<Derived &>(*this); }
-
-  template<typename C>
-  std::string Hash(const C& ops) {
-    for (auto op : ops) {
-      Run(op);
-    }
-    return Get();
-  }
-
-  std::string Hash(Operation *op) {
-    Run(op);
-    return Get();
-  }
-
-  void Print(Operation *op, uint8_t depth) {
-    auto indent = Self().Indent(depth);
-    ss << indent << Self().Op(op);
-    ss << "( ";
-    for (auto o : op->operands) {
-      Print(o, depth + 1);
-      ss << Self().separator;
-    }
-    ss << indent << ")";
-  }
-
-  void Run(Operation *op) {
-    Print(op, 0);
-    ss << "|";
-  }
-
-  std::string Get() { return ss.str(); }
-  std::string Indent(uint8_t) { return {}; }
-};
-
-struct FullNames : Topology<FullNames> {
-  static inline constexpr const char separator = ' ';
-  std::string Op(Operation *op) { return op->Name(); }
-};
-
-struct PrettyPrinter : FullNames {
-  std::string Indent(uint8_t depth) {
-    return std::string(depth * 2, ' ');
-  }
-};
-
-} // namespace print
+static inline std::unordered_set<Operation *> GetContexts(Operation *op) {
+  collect::UpTree<VerifyInstruction> collector;
+  collector.Run(op);
+  return collector.collected;
+}
 
 } // namespace circuitous
 
