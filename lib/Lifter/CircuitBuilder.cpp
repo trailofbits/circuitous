627b0cdeb170199af36f02a0ada560db0d5cc858
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-05-19 21:00:43 +0200

    lifter: Implement Names structure to ease usage of arg name as valid id.
    lifter: Use Names in DependencyVisitor.
    
    lifter: Attempt at implementing error bit and reworking argument handling.

--- lib/Lifter/CircuitBuilder.cpp
+++ lib/Lifter/CircuitBuilder.cpp
@@ -30,6 +30,17 @@
 namespace circuitous {
 namespace {
 
+// `v.name = u.name`
+static inline void CopyName(llvm::Value *v, llvm::Value *u) {
+  v->setName(u->getName());
+}
+
+static inline void CopyArgName(
+    llvm::Function *f, llvm::Function* g, std::size_t i)
+{
+  return CopyName(remill::NthArgument(f, i), remill::NthArgument(g, i));
+}
+
 // TODO(pag): Add other architecture flag names here.
 static const std::string kFlagRegisters =
     ",SF,OF,PF,AF,ZF,CF"  // x86, amd64.
@@ -192,6 +203,28 @@ auto CircuitBuilder::BuildCircuit0(std::vector<InstructionSelection> isels) -> C
   return circuit0;
 }
 
+void CircuitBuilder::FoldOutputChecks() {
+  std::vector<llvm::CallInst *> to_fold;
+
+  for (auto matcher : intrinsics::OutputCheck::All(ctx.module())) {
+    if (!matcher) {
+      continue;
+    }
+    for (auto user : matcher->users()) {
+      if (const auto call_inst = llvm::dyn_cast<llvm::CallInst>(user);
+          call_inst &&
+          call_inst->getArgOperand(0) == call_inst->getArgOperand(1)) {
+        to_fold.push_back(call_inst);
+      }
+    }
+  }
+
+  for (auto call_inst : to_fold) {
+    call_inst->replaceAllUsesWith(llvm::IRBuilder<>(call_inst).getTrue());
+    call_inst->eraseFromParent();
+  }
+}
+
 // Build the second level circuit. Here we analyze how the instruction checkers
 // use registers and try to eliminate unneeded registers from the function's
 // argument list.
@@ -206,81 +239,54 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
   // the current value of a register matches the next expected value of the
   // register.
   intrinsics::VerifyInst::ForAllIn(circuit0_func, [&](llvm::CallInst *verify_call_inst) {
-    auto arg_num = 0u;
-    auto reg_idx = 0u;
-    auto inst_fragments_prefix = inst_fragments_count(verify_call_inst);
-    // TODO(lukas): Refactor, this is clunky at best and very error prone.
     for (auto &arg_use : verify_call_inst->arg_operands()) {
-      llvm::Value *arg = arg_use.get();
-      CHECK(inst_fragments_prefix > 0);
-      if (arg_num < inst_fragments_prefix) {
-        ++arg_num;
+      auto arg = arg_use.get();
+
+      if (!llvm::isa<llvm::CallInst>(arg)) {
+        continue;
+      }
+      auto call = llvm::cast<llvm::CallInst>(arg);
+      auto callee = call->getCalledFunction();
+
+      CHECK(intrinsics::is_any(callee));
+      // We need to do something special only for `OutputCheck`.
+      if (!intrinsics::OutputCheck::IsIntrinsic(callee)) {
         continue;
       }
 
-      const auto call_inst = llvm::dyn_cast<llvm::CallInst>(arg);
-      CHECK(call_inst) << "Expected argument of verify_inst to be calls";
-
-      const auto icmp_eq = call_inst->getCalledFunction();
-      CHECK_NOTNULL(icmp_eq);
-      if (intrinsics::Xor::IsIntrinsic(icmp_eq)) {
-        ++arg_num;
+      auto lhs = call->getArgOperand(0);
+      auto rhs = call->getArgOperand(1);
+      if (!llvm::isa<llvm::Argument>(rhs) || !Names::is_out_reg(rhs)) {
         continue;
       }
-      CHECK(intrinsics::Eq::IsIntrinsic(icmp_eq)) << LLVMName(icmp_eq);
 
-      // Figure out the input and output registers to the circuit function.
-      // const auto reg_id = arg_num - num_instruction_parts;
-      // const auto in_reg_arg_index = num_instruction_parts + (2u * reg_id);
-      const auto in_reg_arg_index = circuit0.inst_bytes.size() + (2u * reg_idx);
-      const auto in_reg_arg =
-          remill::NthArgument(circuit0_func, in_reg_arg_index);
-      const auto out_reg_arg =
-          remill::NthArgument(circuit0_func, in_reg_arg_index + 1u);
-
-      CHECK(out_reg_arg->getName().endswith("_next"))
-          << out_reg_arg->getName().str();
-      CHECK(out_reg_arg->getName().startswith(in_reg_arg->getName()));
-
-      const auto in_reg_name = in_reg_arg->getName().str();
-      const auto out_reg_name = out_reg_arg->getName().str();
-      ++arg_num; ++reg_idx;
-
-      const auto lhs = call_inst->getArgOperand(0);
-      const auto rhs = call_inst->getArgOperand(1);
-
-      const auto rhs_arg = llvm::dyn_cast<llvm::Argument>(rhs);
-      CHECK_EQ(rhs_arg, out_reg_arg)
-          << "Expected second argument to " << icmp_eq->getName().str()
-          << " to match the output register " << out_reg_name << " but got "
-          << remill::LLVMThingToString(rhs)
-          << " instead: " << remill::LLVMThingToString(call_inst);
-
-      // Input is compared to output, i.e. the instruction doesn't write
-      // to this register.
-      if ((llvm::isa<llvm::Argument>(lhs) && lhs == in_reg_arg)) {
+      // We need to check that `rhs` is an output register.
+      CHECK(llvm::isa<llvm::Argument>(rhs) && Names::is_out_reg(rhs))
+          << "Expected second argument to " << callee->getName().str()
+          << " to match the output register"
+          << " instead got: " << remill::LLVMThingToString(call);
+      // We just pass the input into the output -> nothing changes for this reg
+      if (Names::are_tied(lhs, rhs)) {
         continue;
-
-      // Something that wasn't this register's input (might be a different
-      // register's input, or a constant, or a dynamic comparison) is
-      // compared with the output reg, i.e. this instuction writes to the
-      // register.
-      } else if (llvm::isa<llvm::Argument>(lhs) ||
-                 llvm::isa<llvm::Instruction>(lhs) ||
-                 llvm::isa<llvm::ConstantInt>(lhs)) {
-        deps.read_registers.insert(in_reg_arg);
-        deps.written_registers.insert(out_reg_arg);
-        auto &new_reg_val_use = call_inst->getArgOperandUse(0);
+      }
+      if (llvm::isa<llvm::Argument>(lhs) ||
+          llvm::isa<llvm::Instruction>(lhs) ||
+          llvm::isa<llvm::ConstantInt>(lhs)) {
+        deps.read_registers.insert(Names::dual_reg(circuit0_func, rhs));
+        deps.written_registers.insert(llvm::cast<llvm::Argument>(rhs));
+        auto &new_reg_val_use = call->getArgOperandUse(0);
         deps.Visit(new_reg_val_use);
 
       // Some unrecognized pattern.
       } else {
         LOG(FATAL) << "Neither side of integer comparison associated with "
-                   << out_reg_name << " was itself an argument of circuit_0 in "
-                   << remill::LLVMThingToString(call_inst);
+                   << Names::name(rhs).str()
+                   << " was itself an argument of circuit_0 in "
+                   << remill::LLVMThingToString(call);
       }
     }
   });
+  LOG(INFO) << "Gathering parameters of circuit_1";
 
   // Create the type for circuit1. Here, we'll have all output registers come
   // after all input registers.
@@ -288,8 +294,9 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
   std::vector<const remill::Register *> new_regs;
   auto i = 0u;
 
-  circuit1_arg_types.push_back(
-      remill::NthArgument(circuit0_func, 0)->getType());
+  circuit1_arg_types.push_back(remill::NthArgument(circuit0_func, 0)->getType());
+  circuit1_arg_types.push_back(remill::NthArgument(circuit0_func, 1)->getType());
+  circuit1_arg_types.push_back(remill::NthArgument(circuit0_func, 2)->getType());
 
   for (auto in_reg : deps.read_registers) {
     new_regs.push_back(ctx.arch()->RegisterByName(in_reg->getName().str()));
@@ -308,10 +315,15 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
       llvm::FunctionType::get(ir.getInt1Ty(), circuit1_arg_types, false),
       llvm::GlobalValue::ExternalLinkage, "circuit1_func", ctx.module());
   circuit1_func->addFnAttr(llvm::Attribute::ReadNone);
+  LOG(INFO) << "Synthetized circuit_1";
+
+  for (std::size_t i = 0; i < 3; ++i) {
+    CopyArgName(circuit1_func, circuit0_func, i);
+  }
 
   // Rename the parameters to correspond with our input/output registers.
   // i = num_instruction_parts;
-  i = 1u;
+  i = 3u;
   for (auto in_reg : deps.read_registers) {
     remill::NthArgument(circuit1_func, i++)->setName(in_reg->getName());
   }
@@ -328,28 +340,29 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
   std::vector<llvm::Value *> args;
 
   args.push_back(remill::NthArgument(circuit1_func, 0));
+  args.push_back(remill::NthArgument(circuit1_func, 1));
+  args.push_back(remill::NthArgument(circuit1_func, 2));
 
   // Build up an argument list to call circuit0_func from circuit1_func. We
   // pass through the arguments associated with registers that the above
   // analysis determined to be used, and pass in null values (zeroes) for the
   // rest. We'll be able to observe calls like the following:
   //
-  //    %34 = tail call i1 (i8, i8, ...) @intrinsics::Eq(i8 0, i8 0)
+  //    %34 = tail call i1 (i8, i8, ...) @intrinsics::OutputCheck(i8 0, i8 0)
   //
   // Removing uses of these redundant comparisons will let us shrink down the
   // verification call arg lists to only verify used registers.
   for (auto reg : ctx.regs()) {
-    const auto orig_reg = remill::FindVarInFunction(circuit0_func, reg->name);
-    if (auto in_reg = remill::FindVarInFunction(circuit1_func, reg->name, true);
-        in_reg) {
+    auto orig_reg = remill::FindVarInFunction(circuit0_func, Names::as_in_reg(reg->name));
+    if (auto in_reg =
+        remill::FindVarInFunction(circuit1_func, Names::as_in_reg(reg->name), true)) {
       args.push_back(in_reg);
     } else {
       args.push_back(llvm::Constant::getNullValue(orig_reg->getType()));
     }
 
     if (auto out_reg =
-            remill::FindVarInFunction(circuit1_func, reg->name + "_next", true);
-        out_reg) {
+        remill::FindVarInFunction(circuit1_func, Names::as_out_reg(reg->name), true)) {
       args.push_back(out_reg);
     } else {
       args.push_back(llvm::Constant::getNullValue(orig_reg->getType()));
@@ -358,64 +371,55 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
 
   auto entry = llvm::BasicBlock::Create(*ctx.llvm_ctx(), "", circuit1_func);
   auto res = llvm::CallInst::Create(circuit0_func, args, "", entry);
-  (void) llvm::ReturnInst::Create(*ctx.llvm_ctx(), res, entry);
+  llvm::ReturnInst::Create(*ctx.llvm_ctx(), res, entry);
 
   // Optimizing the module again will inline circuit0_func into circuit1_func
   // and propagate the null (i.e. zero) values for all unused registers down
   // through the inlined body of circuit0_func.
   OptimizeSilently(ctx.arch(), ctx.module(), {circuit1_func});
-
-  // "Constant fold" the uses of `__circuitous.icmp_eq.8`.
-  std::vector<llvm::CallInst *> to_fold;
-
-  for (auto matcher : intrinsics::Eq::All(ctx.module())) {
-    if (!matcher) {
-      continue;
-    }
-    for (auto user : matcher->users()) {
-      if (const auto call_inst = llvm::dyn_cast<llvm::CallInst>(user);
-          call_inst &&
-          call_inst->getArgOperand(0) == call_inst->getArgOperand(1)) {
-        to_fold.push_back(call_inst);
-      }
-    }
-  }
-
-  for (auto call_inst : to_fold) {
-    call_inst->replaceAllUsesWith(llvm::IRBuilder<>(call_inst).getTrue());
-    call_inst->eraseFromParent();
-  }
+  FoldOutputChecks();
 
   std::vector<std::pair<llvm::CallInst *, llvm::CallInst *>> to_replace;
 
   // Now go through an change the arguments to `__circuitous.verify_inst` to
   // to reflect the new register transfer comparisons.
   intrinsics::VerifyInst::ForAllIn(circuit1_func, [&](llvm::CallInst *call_inst) {
-    args.clear();
-    auto inst_fragments_prefix = inst_fragments_count<uint32_t>(call_inst);
-    for (auto j = 0u; j < inst_fragments_prefix; ++j) {
-      args.push_back(call_inst->getArgOperand(j));
-    }
-    // TODO(lukas): This is hack, rework.
-    while (true) {
-      using CS = remill::compat::llvm::CallSite;
-      auto op = call_inst->getArgOperand(inst_fragments_prefix);
-      auto cs = CS(llvm::dyn_cast<llvm::Instruction>(op));
-      if (!cs || intrinsics::Eq::IsIntrinsic(cs.getCalledFunction())) {
-        break;
-      }
-      args.push_back(call_inst->getArgOperand(inst_fragments_prefix));
-      ++inst_fragments_prefix;
-    }
-    for (i = 0u; i < ctx.regs().size(); ++i) {
-      const auto reg = ctx.regs()[i];
+    std::vector<llvm::Value *> args;
 
-      // const auto arg = call_inst->getArgOperand(i + num_instruction_parts);
-      const auto arg = call_inst->getArgOperand(i + inst_fragments_prefix);
-      if (std::find(new_regs.begin(), new_regs.end(), reg) != new_regs.end()) {
-        CHECK(!llvm::isa<llvm::Constant>(arg)) << remill::LLVMThingToString(arg);
-        args.push_back(arg);
+    for (auto i = 0u; i < call_inst->getNumArgOperands(); ++i) {
+      using CS = remill::compat::llvm::CallSite;
+      auto op = call_inst->getArgOperand(i);
+      auto cs  = CS(llvm::dyn_cast_or_null<llvm::Instruction>(op));
+      if (op == ir.getTrue()) {
+        continue;
       }
+      if (!cs) {
+        args.push_back(op);
+        continue;
+      }
+      CHECK(cs && intrinsics::is_any(cs.getCalledFunction()))
+          << "We do not expect verify inst to have non-intrinsic operand "
+          << "[ " << i << " ]: "
+          << remill::LLVMThingToString(op);
+      auto callee = cs.getCalledFunction();
+      if (!intrinsics::OutputCheck::IsIntrinsic(callee)) {
+        args.push_back(op);
+      } else {
+        auto op_as_call = llvm::cast<llvm::CallInst>(op);
+        auto lhs = op_as_call->getArgOperand(0);
+        auto rhs = op_as_call->getArgOperand(1);
+        if (llvm::dyn_cast<llvm::Constant>(lhs) &&
+            llvm::dyn_cast<llvm::Constant>(rhs) &&
+            lhs == rhs) {
+          continue;
+        }
+        if (!Names::are_tied(lhs, rhs)) {
+          args.push_back(op);
+        } else {
+          args.push_back(op);
+        }
+      }
+
     }
 
     // TODO(lukas): Make sure we can use `ir` here.
@@ -434,11 +438,9 @@ llvm::Function *CircuitBuilder::BuildCircuit1(Circuit0 circuit0) {
 
 llvm::FunctionType *Circuit0::FnT() {
   llvm::IRBuilder<> ir(*ctx.llvm_ctx());
-  std::vector<llvm::Type *> param_types;
+  Surface{ ctx }.Create(name);
 
-  // First parameter is the bit enconding of the instruction being verified
-  param_types.push_back(ir.getIntNTy(kMaxNumInstBits));
-
+  auto param_types = Surface{ ctx }.Aliens();
     // The remaining parameters will be input/output registers for verification.
   for (auto reg : ctx.regs()) {
     const auto reg_type = IntegralRegisterType(*ctx.module(), reg);
@@ -454,46 +456,12 @@ llvm::Function *Circuit0::GetFn() {
 }
 
 llvm::Function *Circuit0::Create() {
-  llvm::IRBuilder<> ir(*ctx.llvm_ctx());
-  if (circuit_fn) {
-    LOG(FATAL) << "Circuit already has an associated llvm function";
-    return circuit_fn;
-  }
-
-  if (auto fn = GetFn()) {
-    LOG(FATAL) << "The module already has " << name << " function.";
-    return fn;
-  }
-
-  circuit_fn =
-    llvm::Function::Create(FnT(), llvm::GlobalValue::ExternalLinkage, name, ctx.module());
-  circuit_fn->addFnAttr(llvm::Attribute::ReadNone);
-
-  CHECK(circuit_fn->arg_size() > 0);
-  inst_bytes.push_back(&*circuit_fn->arg_begin());
-  // The rest of arguments should be the registers in/out pairs
-  CHECK((circuit_fn->arg_size() - inst_bytes.size()) % 2 == 0);
-  for (auto i = inst_bytes.size(); i < circuit_fn->arg_size(); i += 2) {
-    // This "order" can be flexible, but better let it go in order
-    // of regs.
-    auto reg = ctx.regs()[(i - inst_bytes.size()) / 2];
-
-    auto input_reg = remill::NthArgument(circuit_fn, i);
-    auto output_reg = remill::NthArgument(circuit_fn, i + 1);
-
-    input_reg->setName(reg->name);
-    output_reg->setName(reg->name + "_next");
-
-    reg_to_args.emplace_back(reg, input_reg, output_reg);
-
-    if (reg->name == ctx.arch()->ProgramCounterRegisterName()) {
-      pc = input_reg;
-    }
-  }
+  circuit_fn = surface.Create(name);
 
   // Sanity check, we are going to need pc later on.
-  CHECK(pc != nullptr) << "Couldn't find program counter register "
-                      << ctx.arch()->ProgramCounterRegisterName();
+  CHECK(surface.pc != nullptr)
+      << "Couldn't find program counter register "
+      << ctx.arch()->ProgramCounterRegisterName();
   return circuit_fn;
 }
 
@@ -555,7 +523,7 @@ std::vector<llvm::Value *> Circuit0::HandleDstRegs(
   // register after the semantic has executed matches the next state of that
   // register.
   std::vector<llvm::Value *> params;
-  for (auto [reg, _, expected_reg_val] : reg_to_args) {
+  for (auto [reg, _, expected_reg_val] : surface.reg_to_args) {
     llvm::Value *original_val = state.load(ir, reg);
     llvm::Value *reg_val = original_val;
     // We need to keep track which operand we are about to handle so we can index into
@@ -611,7 +579,7 @@ std::vector<llvm::Value *> Circuit0::HandleDstRegs(
       }
     }
 
-    auto eq_func = intrinsics::Eq::CreateFn(ctx.module(), reg_val->getType());
+    auto eq_func = intrinsics::OutputCheck::CreateFn(ctx.module(), reg_val->getType());
     params.push_back(ir.CreateCall(eq_func, {reg_val, expected_reg_val}));
   }
   return params;
@@ -628,13 +596,13 @@ void Circuit0::InjectSemantic(
   auto state_ptr = state.raw();
   llvm::IRBuilder<> ir(inst_block);
 
-  for (auto [reg, arg, _] : reg_to_args) {
+  for (auto [reg, arg, _] : surface.reg_to_args) {
     state.store(ir, reg, arg);
   }
 
   auto begin = intrinsics::make_breakpoint(ir);
   auto sem_call = CallSemantic(
-    ir, isel.lifted, state_ptr, pc, llvm::UndefValue::get(ctx.memory_ptr_type()));
+    ir, isel.lifted, state_ptr, surface.pc, llvm::UndefValue::get(ctx.memory_ptr_type()));
   auto end = intrinsics::make_breakpoint(ir);
 
   llvm::InlineFunctionInfo info;
@@ -646,14 +614,26 @@ void Circuit0::InjectSemantic(
 
   // Create encoding comparisons
   auto params = ByteFragments(ir, isel);
+
   auto fragments_size = params.size();
+  params.push_back(surface.saturation_property(ir));
+
+  auto [ebit_in, ebit_out] = surface.fetch_ebits();
+  params.push_back(intrinsics::make_outcheck(ir, {ebit_in, ebit_out}));
+
+  auto selects = intrinsics::collect<intrinsics::Select>(begin, end);
 
   // Collect annotated instructions - this is the way separate components
   // of the lfiting pipleline communicate
   auto collected = shadowinst::collect_annotated(begin, end);
   auto dst_intrinsics = std::move(collected["circuitous.dst.reg"]);
+
   auto &extra_params = collected["circuitous.verify_fn_args"];
+  for (std::size_t i = 0; i < extra_params.size(); ++i) {
+    extra_params[i] = intrinsics::Transport::unwrap(extra_params[i]);
+  }
   params.insert(params.end(), extra_params.begin(), extra_params.end());
+
 
   begin->eraseFromParent();
   end->eraseFromParent();
@@ -671,6 +651,7 @@ void Circuit0::InjectSemantic(
   auto call = intrinsics::make_verify(ir, params);
   AddMetadata(call, CircuitBuilder::bytes_fragments_count_kind, fragments_size);
   verified_insts.push_back(call);
+  used_selects.emplace(call, std::move(selects));
 }
 
 std::vector<llvm::Value *> Circuit0::ByteFragments(
@@ -735,6 +716,16 @@ std::vector<llvm::Value *> Circuit0::ByteFragments(
   auto compare = intrinsics::make_bitcompare(ir, {tail, extracted}, tail_size);
   out.push_back(compare);
   return out;
+}
+
+auto Surface::Regs() const -> types_t {
+  types_t params_types;
+  for (auto reg : ctx.regs()) {
+    const auto reg_type = IntegralRegisterType(*ctx.module(), reg);
+    params_types.push_back(reg_type);
+    params_types.push_back(reg_type);
+  }
+  return params_types;
 }
 
 }  // namespace circuitous
