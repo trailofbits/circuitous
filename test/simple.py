fecac7d5b887796ceacf40a5f0af5bc501e3cb71
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-05-24 14:49:05 +0200

    test: Update tags, attempt to create good min set for CI.

--- test/simple.py
+++ test/simple.py
@@ -5,24 +5,18 @@ from byte_generator import intel
 from model_test import ModelTest
 
 test_mov = {
-  Test("mov imm rdx") .bytes("ba12000000").tags({"min", "mov", "broken"})
+  Test("mov imm rdx") .bytes("ba12000000").tags({"mov"})
   .DI(S(0x100).RIP(0x1000))
+  .case(DE = MS().RDX(0x12).RIP(0x1005), R = True)
+  .case(DE = MS().RDX(0x12000000).RIP(0x1005), R = False)
+  .case(DE = MS().RDX(0x13).RIP(0x1005), run_bytes = "ba13000000", R = True)
   .case(
-    DE = MS().RDX(0x12).RIP(0x1005),
-    R = True,
-  ).case(
-    DE = MS().RDX(0x12000000).RIP(0x1005),
-    R = False
-  ).case(
-    DE = MS().RDX(0x13).RIP(0x1005),
-    run_bytes = "ba13000000",
-    R = True
-  ).case(
     I = S(0x0).RIP(0x1000),
     E = S(0x0).RDX(0x13).RIP(0x1005),
     run_bytes = "ff13000000",
     R = False
   ),
+
   Test("mov imm eax/ebx/ecx/edx") \
   .bytes("b812000000bb12000000b912000000ba12000000")
   .tags({"min", "mov"})
@@ -103,18 +97,10 @@ test_mov = {
 }
 
 test_lea = {
-  ModelTest("T:lea ").bytes(intel(["lea rdi, [rsi - 0x15]"])).tags({"min", "lea"}).
-  case("rsi:=0x15",
-    I = S(0x100).RSI(0x15),
-    R = True
-  ).case("rsi:=0x0",
-    I = S(0x100).RSI(0x0),
-    R = True
-  ).
-  case("rsi:=0xffffffffffffff",
-    I = S(0x100).RSI(0xffffffffffffffff),
-    R = True
-  )
+  ModelTest("T:lea ").bytes(intel(["lea rdi, [rsi - 0x15]"])).tags({"lea"})
+  .case("rsi:=0x15", I = S(0x100).RSI(0x15), R = True)
+  .case("rsi:=0x0", I = S(0x100).RSI(0x0), R = True)
+  .case("rsi:=0xffffffffffffff", I = S(0x100).RSI(0xffffffffffffffff), R = True)
 }
 
 test_idiv = {
@@ -136,7 +122,7 @@ test_idiv = {
 
 test_add = {
   ModelTest("T:add rax imm") \
-  .bytes(intel(["add rax, 8"])).tags({'min', "add", "reduce_imms"})
+  .bytes(intel(["add rax, 8"])).tags({"add", "reduce_imms"})
   .case("rax+=8",
     I = S(0x100).RAX(0x5).aflags(0),
     R = True
@@ -146,15 +132,15 @@ test_add = {
     R = True
   ),
   Test("T_reduced:add rax imm") \
-  .bytes(intel(["add rax, 0x10"])).tags({'reduce_imms', 'test2'})
+  .bytes(intel(["add rax, 0x10"])).tags({"reduce_imms", "add"})
   .case("rax+=0x20",
     I = S(0x0).RAX(0x0).aflags(0),
     E = S(0x0).RAX(0x20).RIP(0x1004).aflags(0),
     run_bytes = intel(["add rax, 0x20"])[0],
-    R = True 
+    R = True
   ),
   Test("T_reduced:add rax imm") \
-  .bytes(intel(["add rax, 0x10", "mov rcx, 0x10"])).tags({'reduce_imms', 'test2'})
+  .bytes(intel(["add rax, 0x10", "mov rcx, 0x10"])).tags({"reduce_imms", "add"})
   .case("rax+=0x20",
     I = S(0x0).RAX(0x0).RCX(0x0).aflags(0),
     E = S(0x0).RAX(0x0).RCX(0x20).RIP(0x1004),
@@ -166,7 +152,7 @@ test_add = {
 test_mov_add = {
   ModelTest("T: add, mov") \
   .bytes(intel(["add rax, 0x20", "mov rax, 0x10"]))
-  .tags({'min', 'reduce_imms'})
+  .tags({"reduce_imms", "add", "mov"})
   .DI(S(0x1).RAX(0x0).aflags(0))
   .case("add", run_bytes = 0, R = True)
   .case("mov", run_bytes = 1, R = True),
@@ -175,8 +161,8 @@ test_mov_add = {
   .tags({'reduce_imms', 'test'})
   .DI(S(0x1).RAX(0x0).aflags(0))
   .case("T:add", run_bytes = intel(["add rax, 0x30"])[0], R = True)
-  .case("T:mov", run_bytes = intel(["mov rax, 0x40"])[0], R = True) 
-  .case("F:mov rbx", run_bytes = intel(["mov rbx, 0x40"])[0], R = True) 
+  .case("T:mov", run_bytes = intel(["mov rax, 0x40"])[0], R = True)
+  .case("F:mov rbx", run_bytes = intel(["mov rbx, 0x40"])[0], R = True)
   .case("F:add rbx", run_bytes = intel(["add rbx, 0x40"])[0], R = True),
 }
 
