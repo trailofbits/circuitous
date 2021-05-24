b570f7749f1c95325545c05603b3a13d505f47a3
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-05-24 18:49:45 +0200

    lifter: Add draft of error bit lifting utilities.

---
+++ include/circuitous/Lifter/Error.hpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021 Trail of Bits, Inc.
+ */
+
+#pragma once
+
+#include <circuitous/IR/Intrinsics.hpp>
+#include <circuitous/Util/LLVMUtil.hpp>
+
+#include <tuple>
+#include <vector>
+
+namespace circuitous::err {
+
+  static inline bool is_div(llvm::BinaryOperator *op) {
+    auto op_code = op->getOpcode();
+    using BO = llvm::BinaryOperator;
+    return op_code == BO::SDiv || op_code == BO::UDiv;
+  }
+
+  // Returns `[explicit_errors, implicit_erros]`
+  static inline auto collect(
+      llvm::BasicBlock::iterator begin, llvm::BasicBlock::iterator end)
+  {
+    auto explicit_errs = intrinsics::collect<intrinsics::Error>(begin, end);
+    std::vector<llvm::Value *> implicit_errs;
+    for (;begin != end; ++begin) {
+      if (auto bin_op = llvm::dyn_cast<llvm::BinaryOperator>(&*begin)) {
+        if (is_div(bin_op)) {
+          implicit_errs.push_back(bin_op);
+        }
+      }
+    }
+    return std::make_tuple(std::move(explicit_errs), std::move(implicit_errs));
+  }
+
+  static inline auto collect(llvm::Value *from, llvm::Value *to) {
+    CHECK(llvm::isa<llvm::Instruction>(from) && llvm::isa<llvm::Instruction>(to));
+
+    auto as_it = [](auto what) {
+      return llvm::BasicBlock::iterator{ llvm::cast<llvm::Instruction>(what) };
+    };
+    return collect(as_it(from), as_it(to));
+  }
+
+
+  static inline auto handle_implicit(
+      llvm::IRBuilder<> &ir, const std::vector<llvm::Value *> &implicit_errs)
+  {
+    std::vector<llvm::Value *> out;
+    return out;
+  }
+
+  static inline llvm::Value *synthesise_current(
+      llvm::IRBuilder<> &ir, llvm::Value *from, llvm::Value *to)
+  {
+    const auto &[explicit_errs, implicit_errs] = collect(from, to);
+
+    // Check if this instruction is faultable
+    if (explicit_errs.empty() && implicit_errs.empty()) {
+      return nullptr;
+    }
+
+    auto args = intrinsics::Error::unwrap(explicit_errs);
+
+    auto args_ = handle_implicit(ir, implicit_errs);
+    args.insert(args.end(), args_.begin(), args_.end());
+    return intrinsics::make_or(ir, args);
+  }
+
+} // namespace circuitous::err
