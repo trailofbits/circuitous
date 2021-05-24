f02c35df489af79c3961957865a016171d6ff29a
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-05-24 18:45:54 +0200

    print: Add new more simple version of dot printer.

--- lib/Printers/DOT.cpp
+++ lib/Printers/DOT.cpp
@@ -7,6 +7,16 @@
 #include <ostream>
 #include <unordered_map>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wconversion"
+#include <glog/logging.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Module.h>
+#pragma clang diagnostic pop
+
 namespace circuitous {
 namespace {
 
@@ -67,11 +77,96 @@ class DOTPrinter : public UniqueVisitor<DOTPrinter> {
   const value_map_t &node_values;
 };
 
+
 }  // namespace
+
+namespace dot {
+
+struct Printer : UniqueVisitor<Printer> {
+  using value_map_t = std::unordered_map<Operation *, std::string>;
+  explicit Printer(std::ostream &os_, const value_map_t &vals)
+    : os(os_), node_values(vals) {}
+
+
+  std::string Operand(Operation *of, std::size_t i) {
+    return NodeID(of) + ':' + NodeID(of) + std::to_string(i);
+  }
+
+  void Edge(Operation *from, Operation *to, std::size_t i) {
+    os << Operand(from, i)
+       << " -> "
+       << NodeID(to)
+       << ";\n";
+  }
+
+  std::string NodeID(Operation *op) {
+    return "v" + std::to_string(op->id()) + "v";
+  }
+
+  std::string AsID(const std::string &what) {
+    return "<" + what + ">";
+  }
+
+  void Node(Operation *op) {
+
+    if (auto lop = dynamic_cast<LLVMOperation *>(op); lop && lop->llvm_op_code == llvm::BinaryOperator::Add) {
+      os << NodeID(op) << " [fillcolor=red;style=filled;label = " << '"'
+        << "{ " << AsID(NodeID(op)) << " " << op->Name();
+    } else {
+      os << NodeID(op) << " [label = " << '"'
+        << "{ " << AsID(NodeID(op)) << " " << op->Name();
+    }
+    if (node_values.count(op)) {
+      os << " " << node_values.find(op)->second << " ";
+    }
+
+    if (op->operands.size() == 0) {
+      os << " }" << '"' << "];\n";
+      return;
+    }
+
+    os << "| {";
+    for (std::size_t i = 0; i < op->operands.size(); ++i) {
+      os << AsID(NodeID(op) + std::to_string(i));
+      if (i != op->operands.size() - 1) {
+        os << " | ";
+      }
+    }
+    os << " }}" << '"' << "];\n";
+  }
+
+  void Init() {
+    os << "digraph {" << std::endl;
+    os << "node [shape=record];";
+  }
+
+  void VisitOperation(Operation *op) {
+    op->Traverse(*this);
+    Node(op);
+    for (std::size_t i = 0; i < op->operands.size(); ++i) {
+      Edge(op, op->operands[i], i);
+    }
+  }
+
+  void VisitCircuit(Circuit *op) {
+    Init();
+    op->Traverse(*this);
+    Node(op);
+    for (std::size_t i = 0; i < op->operands.size(); ++i) {
+      Edge(op, op->operands[i], i);
+    }
+    os << "}";
+  }
+
+  std::ostream &os;
+  const value_map_t &node_values;
+};
+
+} // namespace dot
 
 void PrintDOT(std::ostream &os, Circuit *circuit,
               const std::unordered_map<Operation *, std::string> &node_values) {
-  circuitous::DOTPrinter dot_os(os, node_values);
+  circuitous::dot::Printer dot_os(os, node_values);
   dot_os.Visit(circuit);
 }
 
