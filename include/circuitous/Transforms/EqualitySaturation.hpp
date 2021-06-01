c1ade0419e648985a78c0b52f5a9b833092a5d8e
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-06-01 15:58:30 +0200

    eqsat: Clean up pattern matching.

--- include/circuitous/Transforms/EqualitySaturation.hpp
+++ include/circuitous/Transforms/EqualitySaturation.hpp
@@ -4,16 +4,14 @@
 
 #pragma once
 
+
 #include <vector>
 #include <string>
+#include <string_view>
+
+#include <circuitous/Transforms/Pattern.hpp>
 
 namespace circuitous {
-
-  template < typename Graph >
-  struct Pattern
-  {
-
-  };
 
   // substitution mapping from varibles to equality classes
   template< typename Graph >
@@ -37,41 +35,57 @@ namespace circuitous {
   template< typename Graph >
   struct Rule
   {
+    using Pattern = Pattern< Graph >;
     using EClassMatches = EClassMatches< Graph >;
     using Matches = std::vector< EClassMatches >;
 
+    Rule(std::string_view name, std::string_view lhs, std::string_view rhs)
+      : name(name), _lhs(Pattern(lhs)), _rhs(Pattern(rhs))
+    {}
+
     struct Matcher
     {
+      Matcher(Pattern pattern) : _pattern(pattern) {}
+
       Matches match(const Graph &egraph) const
       {
         return {};
       }
+
+    private:
+      Pattern _pattern;
     };
 
     struct Applier
     {
+      Applier(Pattern pattern) : _pattern(pattern) {}
+
       void apply_on_matches(const Graph &egraph, const Matches &matches) const
       {
 
       }
+
+    private:
+      Pattern _pattern;
     };
-
-    const std::string name;
-
-    // Rewrite rule 'lhs -> rhs' that allows to match
-    // left-hand-side and replace it with right-hand-side
-    Matcher lhs;
-    Applier rhs;
 
     Matches match(const Graph &egraph) const
     {
-      return lhs.match(egraph);
+      return _lhs.match(egraph);
     }
 
     void apply(const Graph &egraph, const Matches &matches) const
     {
-      rhs.apply_on_matches(egraph, matches);
+      _rhs.apply_on_matches(egraph, matches);
     }
+
+    const std::string name;
+
+  private:
+    // Rewrite rule 'lhs -> rhs' that allows to match
+    // left-hand-side and replace it with right-hand-side
+    Matcher _lhs;
+    Applier _rhs;
   };
 
   template< typename Graph >
