248a97aee3d9e4ad80e0f6c061047c34eb7bda58
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-06-07 16:30:06 +0200

    test: Update pattern parser tests.

--- test/unit/Transforms/Pattern.cpp
+++ test/unit/Transforms/Pattern.cpp
@@ -13,52 +13,51 @@
 
 namespace circuitous {
 
-  using parser_t = PatternParser<int>;
-  using list_t = parser_t::List;
-  using error_t = parser_t::Error;
-  using place_t = parser_t::Place;
-  using op_t = parser_t::Op;
+  using Parser = PatternParser<int>;
+  using Place = Parser::Place;
+  using Op = Parser::Op;
 
   template< typename T, typename Parsed  >
-  T get(const Parsed &val)
+  T get(const Parsed &node)
   {
-    CHECK(std::holds_alternative< T >(val));
-    return std::get<T>(val);
+    return std::get<T>(node->value);
   }
 
   template< typename T, typename Parsed, unsigned I, unsigned ...Is >
   T get(const Parsed &val)
   {
-    CHECK(std::holds_alternative<list_t>(val));
-    auto list = std::get<list_t>(val);
-    return get< T >(list[I], Is...);
+    return get< T, Parsed, Is... >(val->children[I]);
   }
   template< typename T, unsigned ...Indices >
-  T parse(const parser_t &p, std::string_view str)
+  T parse(const Parser &p, std::string_view str)
   {
     auto val = p.parse(str);
     return get< T, decltype(val), Indices... >(val);
   }
 
-  bool fails(const parser_t &parser, std::string_view str)
+  bool fails(const Parser &parser, std::string_view str)
   {
-    auto val = parser.parse(str);
-    return std::holds_alternative<error_t>(val);
+    return parser.parse(str) == nullptr;
   }
 
   TEST_CASE("Pattern Simple Parse") {
-    parser_t parser;
+    Parser parser;
 
-    CHECK(parse<int>(parser, "5") == 5);
-    CHECK(parse<int>(parser, "-15") == -15);
+    CHECK(parse< int >(parser, "5") == 5);
+    CHECK(parse< int >(parser, "-15") == -15);
 
-    CHECK(parse<int, 0>(parser, "(1 2 3)") == 1);
-    CHECK(parse<int, 1>(parser, "(1 2 3)") == 2);
-    CHECK(parse<int, 2>(parser, "(1 2 3)") == 3);
+    CHECK(parse< Op >(parser, "(op_add 1 2)").name == "add");
+    CHECK(parse< int, 0 >(parser, "(op_add 1 2)") == 1 );
+    CHECK(parse< int, 1 >(parser, "(op_add 1 2)") == 2 );
 
-    CHECK(parse<op_t, 0>(parser, "(op_add 1 2)").name == "add");
+    CHECK(parse< Place >(parser, "?x").name == "x");
 
-    CHECK(parse<place_t>(parser, "?x").name == "x");
+    CHECK(parse< Op >(parser, "(op_add (op_mul 1 ?x) 3)").name == "add" );
+    CHECK(parse< Op, 0 >(parser, "(op_add (op_mul 1 ?x) 3)").name == "mul" );
+    CHECK(parse< int, 0, 0 >(parser, "(op_add (op_mul 1 ?x) 3)") == 1 );
+    CHECK(parse< Place, 0, 1 >(parser, "(op_add (op_mul 1 ?x) 3)").name == "x" );
+    CHECK(parse< int, 1 >(parser, "(op_add (op_mul 1 ?x) 3)") == 3 );
+
     CHECK(fails(parser, "?-7"));
     CHECK(fails(parser, "?7"));
 
