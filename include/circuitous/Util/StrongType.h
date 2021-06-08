aa1a124de9d825da5be66b8335582c73740d0d60
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-06-08 13:07:28 +0200

    util: Add strong type wrapper.

---
+++ include/circuitous/Util/StrongType.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021 Trail of Bits, Inc.
+ */
+
+#pragma once
+
+#include <compare>
+
+namespace circuitous {
+
+  template< typename underlying_t, typename tag >
+  struct strong_type
+  {
+    constexpr strong_type() : _value() {}
+
+    explicit constexpr strong_type(const underlying_t &value) : _value(value) {}
+    explicit constexpr strong_type(underlying_t &&value)
+        noexcept( std::is_nothrow_move_constructible_v<underlying_t> )
+      : _value(std::move(value))
+    {}
+
+    explicit constexpr operator underlying_t&() noexcept { return _value; }
+    explicit constexpr operator const underlying_t&() const noexcept { return _value; }
+    explicit constexpr operator underlying_t&&() noexcept { return std::move(_value); }
+
+    constexpr underlying_t& ref() noexcept { return _value; }
+    constexpr const underlying_t& ref() const noexcept { return _value; }
+
+    friend void swap(strong_type &a, strong_type &b) noexcept
+    {
+      using std::swap;
+      swap(static_cast<underlying_t&>(a), static_cast<underlying_t&>(b));
+    }
+
+    friend bool operator==(const strong_type &a, const strong_type &b) = default;
+    friend bool operator!=(const strong_type &a, const strong_type &b) = default;
+
+    friend bool operator==(const strong_type &a, const underlying_t &b) { return a._value == b; }
+    friend bool operator!=(const strong_type &a, const underlying_t &b) { return !(a == b); }
+
+    friend bool operator==(const underlying_t &a, const strong_type &b) { return a == b._value; }
+    friend bool operator!=(const underlying_t &a, const strong_type &b) { return !(a == b); }
+
+    template< typename stream >
+    friend stream& operator<<(stream &out, const strong_type &a) noexcept
+    {
+      return out << a._value;
+    }
+
+  private:
+    underlying_t _value;
+  };
+} // namespace circuitous
+
+namespace std {
+  template< typename underlying_t, typename tag >
+  struct hash< circuitous::strong_type< underlying_t, tag > >
+  {
+    using underlying_hash = std::hash<underlying_t>;
+
+    size_t operator()(const circuitous::strong_type< underlying_t, tag > &a) const noexcept
+    {
+      return underlying_hash{}(static_cast< const underlying_t& >(a));
+    }
+  };
+}
