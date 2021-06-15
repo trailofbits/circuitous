6a68b6046f80e1345d696af0f58c4799f26c397c
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-06-15 13:45:31 +0200

    test: Deduplicate egraph constants.

--- test/unit/lib/support/support/EGraph.hpp
+++ test/unit/lib/support/support/EGraph.hpp
@@ -14,30 +14,50 @@
 
 #include <fstream>
 #include <string>
+#include <map>
 
 namespace circuitous {
 
   template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
   template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-  struct TestGraph : EGraph< ENode< std::string> >
+  using StringNode = ENode< std::string >;
+  struct TestGraph : EGraph< StringNode >
   {
     auto make_leaf(const std::string &expr)
     {
-      return add(ENode(expr));
+      auto candidate = ENode(expr);
+      auto constant = candidate.constant();
+
+      if (constant) {
+        if (auto it = constants.find(constant.value()); it != constants.end()) {
+          return find(it->second);
+        }
+      }
+
+      auto [id, node] = add(std::move(candidate));
+
+      if (constant) {
+        constants.emplace(constant.value(), node);
+      }
+
+      return id;
     }
 
     auto make_node(const std::string &expr, std::vector<Id> children)
     {
       ENode node(expr);
       node.children = std::move(children);
-      return add(std::move(node));
+      auto [id, _] = add(std::move(node));
+      return id;
     }
 
     void dump(const std::string &file) {
       std::ofstream out(file);
       to_dot(*this, out, [] (auto *node) { return node->term; });
     }
+
+    std::map<std::int64_t, StringNode*> constants;
   };
 
   using TestENode = TestGraph::ENode;
