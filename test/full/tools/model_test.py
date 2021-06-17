1ffe2d05a8bdb60f536788a69991e215d71599aa
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-06-17 22:08:45 +0200

    test: Update microx based test generator to generate memory hints.

--- test/full/tools/model_test.py
+++ test/full/tools/model_test.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2021 Trail of Bits, Inc.
 
 import copy
+import math
 
-from tools.tc import State, Test, _regs, _aflags, MS
+from tools.tc import State, Test, _regs, _aflags, MS, MemHint
 
 import microx
 from microx_core import Executor
@@ -31,6 +32,31 @@ class ModelTest(Test):
       case.expected.result = result
     return self
 
+
+# Watch out, exceptions works funky
+class Process_(microx.Process):
+
+  def __init__(self, ops, mem):
+    super(Process_, self).__init__(ops, mem)
+    self._mem_hints = []
+
+  def write_memory(self, addr, data):
+    as_str = str(self._ops.convert_to_integer(data))
+    hint = MemHint(addr, int(as_str), MemHint.WRITE, len(data))
+    self._mem_hints.append(hint)
+
+    return super(Process_, self).write_memory(addr, data)
+
+  def read_memory(self, addr, num_bytes, hint):
+    read = super(Process_, self).read_memory(addr, num_bytes, hint)
+    as_str = str(self._ops.convert_to_integer(read))
+
+    if hint in self.MEM_READ_HINTS and hint not in self.MEM_EXEC_HINTS:
+      hint_ = MemHint(addr, int(as_str), MemHint.READ, len(read))
+      self._mem_hints.append(hint_)
+
+    return super(Process_, self).read_memory(addr, num_bytes, hint)
+
 class MicroxGen:
   def __init__(self):
     pass
@@ -40,14 +66,32 @@ class MicroxGen:
     # assert "RSP" not in input.registers
 
     rip = input.registers.get("RIP", 0x1000)
-    size = 0x1000
+    rsp = input.registers.get("RSP")
 
     o = microx.Operations()
+    m = microx.Memory(o, 64)
+
+    size = 0x1000
     code = microx.ArrayMemoryMap(o, rip, rip + size, can_write=False, can_execute=True)
+    m.add_map(code)
+
+    #TODO(lukas): Stack & other memory
+    if input.memory is not None:
+      for region in input.memory.entries:
+        addr, bytes, (r, w, e) = region
+        assert len(bytes) < size
+        aligned_addr = (addr >> 12) << 12
+        amm = microx.ArrayMemoryMap(o, aligned_addr, aligned_addr + size,
+                                    can_read=r, can_write=w, can_execute=e)
+        m.add_map(amm)
+        amm.store_bytes(addr, bytes)
+
+    MS()
+
     bytes = []
     for i in range(0, len(input.bytes), 2):
       bytes.append(int(input.bytes[i] + input.bytes[i + 1], 16))
-    code.store_bytes(0x1000, bytes)
+    code.store_bytes(rip, bytes)
 
     t = microx.EmptyThread(o)
     for reg, val in input.registers.items():
@@ -55,9 +99,7 @@ class MicroxGen:
         t.write_register(reg, val)
     t.write_register("RIP", rip)
 
-    m = microx.Memory(o, 64)
-    m.add_map(code)
-    e = microx.Process(o, m)
+    e = Process_(o, m)
 
     out = State()
 
@@ -67,6 +109,10 @@ class MicroxGen:
       out = copy.deepcopy(input)
       out.ebit(True)
       return out
+    out.timestamp = input.timestamp + 1
+
+    for mem_hint in e._mem_hints:
+      out.mem_hint(mem_hint)
 
     for reg in _regs + _aflags:
       out.set_reg(reg, t.read_register(reg, t.REG_HINT_NONE))
