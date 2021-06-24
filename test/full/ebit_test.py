a14d51bad006e6dae581cbc0ea3277ba76b81a0a
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-06-25 00:03:39 +0200

    test: Add more memory tests.

--- test/full/ebit_test.py
+++ test/full/ebit_test.py
@@ -3,7 +3,7 @@
 from tools.byte_generator import intel
 from tools.model_test import ModelTest
 from tools.verify_test import VerifyTest
-from tools.tc import State, S, MS
+from tools.tc import State, S, MS, MIG
 
 _verify_test = {
   VerifyTest("selftest").tags({"selftest"})
@@ -73,19 +73,72 @@ push_cx = {
 }
 
 push_deref = {
-  ModelTest("push_deref").tags({"push", "min", "mem", "todo"})
+  ModelTest("push_deref").tags({"push", "min", "mem"})
   .bytes(intel(["push -0x5[rax]"]))
-  .DI(S(0x20040).RAX(0x50056).RIP(0x1000).RDX(0x70070).RBX(0x40040)
+  .DI(S(0x20040).RAX(0x50056).RIP(0x1000).RDX(0x70070).RBX(0x40040).RSP(0x9008)
                 .rwmem(0x40030, "3132e3" * 25)
-                .rwmem(0x50050, "ff" * 25)
+                .rwmem(0x50048, "afb14c" * 64)
                 .rwmem(0x40030, "ee" * 100)
-                .rwmem(0x70000, "ba12" * 100))
+                .rwmem(0x70000, "ba12" * 100)
+                .rwmem(0x9008, "a125be" * 25))
   .case(run_bytes = 0, R = True)
-  .case(run_bytes = intel(["push [rax]"]), R = True)
+  .case(run_bytes = intel(["push 0x4[rax]"]), R = True)
   .case(run_bytes = intel(["push 0x5[rbx]"]), R = True)
   .case(run_bytes = intel(["push -0x35[rdx]"]), R = True)
   .case(run_bytes = intel(["push [rax]"]), R = False)
 }
 
+mov_mem = {
+  ModelTest("mov reg, dreg").tags({"mov", "min", "mem"})
+  .bytes(intel(["mov rax, [rbx]"]))
+  .DI(S(0x20040).wrm_RAX(0x50056, 65).wrm_RBX(0x4008, 0x100).wrm_RAX(0x9800, 0x100))
+  .case(run_bytes = 0, R=True)
+  .case(run_bytes = intel(["mov [rax], rbx"]), R=False)
+  .case(run_bytes = intel(["mov rdx, [rax]"]), R=True),
+
+  ModelTest("mov dreg, reg").tags({"mov", "min", "mem"})
+  .bytes(intel(["mov [rax], rbx"]))
+  .DI(S(0x20040).wrm_RAX(0x50056, 65).wrm_RBX(0x4008, 0x100).wrm_RDX(0x9800, 0x100))
+  .case(run_bytes = 0, R=True)
+  .case(run_bytes = intel(["mov rax, [rbx]"]), R=False)
+  .case(run_bytes = intel(["mov [rdx], rax"]), R=True),
+
+  ModelTest("rex prefix").tags({"mov", "mem", "min"})
+  .bytes(intel(["mov [r9], rax", "mov rax, [r9]"]))
+  .DI(S(0x30048).wrm_RAX(0x30054, 0x100).wrm_RDX(0x40032, 0x100)
+                .wrm_R9(0x60421, 0x100).wrm_R8(0x70212, 0x100))
+  .case(run_bytes = 0, R=True)
+  .case(run_bytes = 1, R=True)
+  .scases(intel, ["mov [r8], r9", "mov [rdx], r8", "mov [r9], rax"], R=True),
+
+  ModelTest("displacement").tags({"mov", "mem", "min"})
+  .bytes(intel(["mov [rax + 2 * r9 - 0x5], rdx", "mov rax, [rcx + 2 * r9 + 0x4]"]))
+  .DI(S(0x300049).wrm_RAX(0x8000).wrm_RBX(0x2000).wrm_RCX(0x3000).wrm_RSI(0x4000)
+                 .wrm_R8(0x5000).wrm_R9(0x6000).wrm_R10(0x7000)
+                 .mig(MIG().rw(0x13000, 0x1000)
+                           .rw(0x14000, 0x1000)
+                           .rw(0xe000, 0x1000)
+                           .rw(0x12000, 0x1000)
+                           .rw(0xd000, 0x1000)
+                      )
+      )
+  .case(run_bytes = 0, R=True)
+  .case(run_bytes = 0, R=True)
+  .scases(intel, ["mov rax, [r9 + 2*rsi -0x5]", "mov r10, [rsi + 2 * r10 + 0x5]"], R=True)
+  .scases(intel, ["mov [r9 + 2*rsi -0x5], rcx", "mov [rsi + 2 * r10 + 0x5], r8"], R=True)
+}
+
+xor_mem = {
+  ModelTest("xor mem reg").tags({"xor", "mem", "min"})
+  .bytes(intel(["xor [rax], rbx"]))
+  .DI(S(0x2005).wrm_RAX(0x324d).wrm_RBX(0x4521).wrm_RSI(0x51b1).wrm_RDX(0x6ac1)
+               .wrm_R8(0x7128))
+  .case(run_bytes = 0, R=True)
+  .scases(intel, ["xor [rbx], rdx", "xor [rsi], rsi", "xor [rdx], rax"], R=True)
+  .case(run_bytes = intel(["xor [r8], rax"]), R=True)
+}
+
+
+
 circuitous_tests = [ saturation_property, _verify_test, timestamp_inc,
-  push, pop, push_cx, push_deref ]
+  push, pop, push_cx, push_deref, mov_mem, xor_mem ]
