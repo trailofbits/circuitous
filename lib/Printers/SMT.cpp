a75445b36f508b707addcaf2255895ce5622c574
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-07-21 18:27:25 +0200

    ir: Update bitblasted smt output printer.

--- lib/Printers/SMT.cpp
+++ lib/Printers/SMT.cpp
@@ -7,10 +7,10 @@
 
 namespace circ
 {
-
-  void PrintSMT(std::ostream &os, Circuit *circuit, auto visitor)
+  void PrintSMT(std::ostream &os, Circuit *circuit)
   {
     try {
+      auto visitor = IRToSMTVisitor();
       auto expr = visitor.Visit(circuit);
       z3::solver solver(visitor.ctx);
       solver.add(expr);
@@ -21,14 +21,38 @@ namespace circ
     }
   }
 
-  void PrintSMT(std::ostream &os, Circuit *circuit)
+  z3::solver bitblast(z3::expr expr, z3::context &ctx)
   {
-    PrintSMT(os, circuit, IRToSMTVisitor());
+    z3::solver solver(ctx);
+
+    z3::tactic tactic(
+      z3::tactic(ctx, "ctx-simplify") &
+      z3::tactic(ctx, "propagate-bv-bounds") &
+      z3::tactic(ctx, "solve-eqs") &
+      z3::tactic(ctx, "simplify") &
+      z3::tactic(ctx, "bit-blast") &
+      z3::tactic(ctx, "aig")
+    );
+
+    z3::goal goal(ctx);
+    goal.add(expr);
+    expr = tactic(goal)[0].as_expr();
+
+    // Custom optimizations
+
+    solver.add(expr);
+    return solver;
   }
 
   void PrintBitBlastSMT(std::ostream &os, Circuit *circuit)
   {
-    PrintSMT(os, circuit, IRToBitBlastableSMTVisitor());
+    try {
+      auto visitor = IRToBitBlastableSMTVisitor();
+      auto expr = visitor.Visit(circuit);
+      os << bitblast(expr, visitor.ctx).to_smt2() << '\n';
+    } catch (const z3::exception &e) {
+      LOG(FATAL) << e.what() << '\n';
+    }
   }
 
 }  // namespace circ
