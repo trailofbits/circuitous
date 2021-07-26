117843d96e71911da002fafea650da1f546cfb66
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-07-26 13:34:10 +0200

    ir: Implement smt bit-blasting statistics.

--- lib/Printers/SMT.cpp
+++ lib/Printers/SMT.cpp
@@ -21,35 +21,13 @@ namespace circ
     }
   }
 
-  z3::solver bitblast(z3::expr expr, z3::context &ctx)
-  {
-    z3::solver solver(ctx);
-
-    z3::tactic tactic(
-      z3::tactic(ctx, "ctx-simplify") &
-      z3::tactic(ctx, "propagate-bv-bounds") &
-      z3::tactic(ctx, "solve-eqs") &
-      z3::tactic(ctx, "simplify") &
-      z3::tactic(ctx, "bit-blast") &
-      z3::tactic(ctx, "aig")
-    );
-
-    z3::goal goal(ctx);
-    goal.add(expr);
-    expr = tactic(goal)[0].as_expr();
-
-    // Custom optimizations
-
-    solver.add(expr);
-    return solver;
-  }
-
   void PrintBitBlastSMT(std::ostream &os, Circuit *circuit)
   {
     try {
       auto visitor = IRToBitBlastableSMTVisitor();
       auto expr = visitor.Visit(circuit);
-      os << bitblast(expr, visitor.ctx).to_smt2() << '\n';
+      auto bitblasted = bitblast(expr, visitor.ctx);
+      os << bitblasted.to_smt2() << '\n';
     } catch (const z3::exception &e) {
       LOG(FATAL) << e.what() << '\n';
     }
