5e1d67c131159392ac6bc18fc26857152b7a78b8
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-21 13:09:46 +0200

    test: Add variadic match eqsat match tests.

--- test/unit/Transforms/EqualitySaturation.cpp
+++ test/unit/Transforms/EqualitySaturation.cpp
@@ -444,6 +444,127 @@ namespace circ::eqsat {
       CHECK(bondctx == contexts(egraph, mul1));
       CHECK(bondctx == contexts(egraph, mul2));
     }
+
+    TEST_CASE("Variadic Match")
+    {
+      TestGraph egraph;
+      TestGraphBuilder builder(&egraph);
+
+      auto a = egraph.make_leaf("a");
+      auto b = egraph.make_leaf("b");
+      auto c = egraph.make_leaf("c");
+      auto d = egraph.make_leaf("d");
+      auto e = egraph.make_leaf("e");
+      auto f = egraph.make_leaf("f");
+
+      auto mul1 = egraph.make_node("mul", {a, b});
+      auto mul2 = egraph.make_node("mul", {c, d});
+      auto mul3 = egraph.make_node("mul", {e, f});
+
+      egraph.make_node("CTX1", {mul1});
+      egraph.make_node("CTX2", {mul2});
+      egraph.make_node("CTX3", {mul3});
+
+      auto rule = TestRule(
+        "variadic-bond-multiplications",
+        "((let M (op_mul)) (commutative-match $M...))",
+        "(bond $M...)"
+      );
+
+      auto matches = rule.match(egraph);
+      CHECK(matches.size() == 1);
+
+      rule.apply(egraph, builder);
+      egraph.rebuild();
+
+      CHECK(egraph.bonded({mul1, mul2, mul3}));
+
+      auto bond = egraph.bonded({mul1, mul2, mul3}).value();
+      auto bondctx = contexts(egraph, bond);
+      CHECK(bondctx == contexts(egraph, mul1));
+      CHECK(bondctx == contexts(egraph, mul2));
+      CHECK(bondctx == contexts(egraph, mul3));
+    }
+
+    TEST_CASE("Variadic N-ary Match")
+    {
+      TestGraph egraph;
+      TestGraphBuilder builder(&egraph);
+
+      auto a = egraph.make_leaf("a");
+      auto b = egraph.make_leaf("b");
+      auto c = egraph.make_leaf("c");
+      auto d = egraph.make_leaf("d");
+      auto e = egraph.make_leaf("e");
+      auto f = egraph.make_leaf("f");
+      auto g = egraph.make_leaf("e");
+      auto h = egraph.make_leaf("f");
+
+      auto mul1 = egraph.make_node("mul", {a, b});
+      auto mul2 = egraph.make_node("mul", {c, d});
+      auto add1 = egraph.make_node("add", {e, f});
+      auto add2 = egraph.make_node("add", {g, h});
+
+      egraph.make_node("CTX1", {mul1});
+      egraph.make_node("CTX2", {mul2});
+      egraph.make_node("CTX3", {add1});
+      egraph.make_node("CTX4", {add2});
+
+      auto rule = TestRule(
+        "variadic-bond-operations",
+        "((let M (op_mul)) (let A (op_add)) (commutative-match $M... $A...))",
+        "(bond $M... $A...)"
+      );
+
+      auto matches = rule.match(egraph);
+      CHECK(matches.size() == 1);
+
+      rule.apply(egraph, builder);
+      egraph.rebuild();
+
+      CHECK(egraph.bonded({mul1, mul2, add1, add2}));
+    }
+
+    TEST_CASE("Variadic Unify")
+    {
+      TestGraph egraph;
+      TestGraphBuilder builder(&egraph);
+
+      auto a = egraph.make_leaf("a");
+      auto b = egraph.make_leaf("b");
+      auto c = egraph.make_leaf("c");
+      auto d = egraph.make_leaf("d");
+      auto e = egraph.make_leaf("e");
+      auto f = egraph.make_leaf("f");
+      auto g = egraph.make_leaf("e");
+      auto h = egraph.make_leaf("f");
+
+      auto mul1 = egraph.make_node("mul", {a, b});
+      auto mul2 = egraph.make_node("mul", {c, d});
+      auto add1 = egraph.make_node("add", {e, f});
+      auto add2 = egraph.make_node("add", {g, h});
+
+      egraph.make_node("CTX1", {mul1});
+      egraph.make_node("CTX2", {mul2});
+      egraph.make_node("CTX3", {add1});
+      egraph.make_node("CTX4", {add2});
+
+      auto rule = TestRule(
+        "variadic-bond-operations",
+        "((let M (op_mul)) (let A (op_add)) (commutative-match $M... $A...))",
+        "(union $M... $A...)"
+      );
+
+      auto matches = rule.match(egraph);
+      CHECK(matches.size() == 1);
+
+      rule.apply(egraph, builder);
+      egraph.rebuild();
+
+      CHECK(egraph.eclass(mul1) == egraph.eclass(mul2));
+      CHECK(egraph.eclass(mul2) == egraph.eclass(add1));
+      CHECK(egraph.eclass(add1) == egraph.eclass(add2));
+    }
   }
 
 } // namespace circ::eqsat
