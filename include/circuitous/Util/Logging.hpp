b3c0ff54bdb30324f471ef072f80de1b2707b926
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-28 15:46:46 +0200

    util: Add a few more options to relaxed warnings.

--- include/circuitous/Util/Logging.hpp
+++ include/circuitous/Util/Logging.hpp
@@ -7,7 +7,9 @@
 #define CIRCUITOUS_RELAX_WARNINGS \
   _Pragma( "clang diagnostic push" ) \
   _Pragma( "clang diagnostic ignored \"-Wsign-conversion\"" ) \
-  _Pragma( "clang diagnostic ignored \"-Wconversion\"" )
+  _Pragma( "clang diagnostic ignored \"-Wconversion\"" ) \
+  _Pragma( "clang diagnostic ignored \"-Wold-style-cast\"" ) \
+  _Pragma( "clang diagnostic ignored \"-Wnon-virtual-dtor\"" )
 
 #define CIRCUITOUS_UNRELAX_WARNINGS \
   _Pragma( "clang diagnostic pop" ) \
