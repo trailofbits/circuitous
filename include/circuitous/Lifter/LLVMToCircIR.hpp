631a3d2173627b3a23e110c60c1f3b03494cf69f
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-02-09 07:37:04 +0100

    lifter: Add header to expose lowering pass from llvm to circ ir.

---
+++ include/circuitous/Lifter/LLVMToCircIR.hpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022 Trail of Bits, Inc.
+ */
+
+#include <circuitous/IR/Circuit.hpp>
+#include <circuitous/Lifter/Context.hpp>
+#include <circuitous/Util/Logging.hpp>
+
+CIRCUITOUS_RELAX_WARNINGS
+#include <llvm/IR/Module.h>
+CIRCUITOUS_UNRELAX_WARNINGS
+
+namespace circ
+{
+    Circuit::circuit_ptr_t lower_fn(llvm::Function *, Ctx &ctx);
+} // namespace circ
