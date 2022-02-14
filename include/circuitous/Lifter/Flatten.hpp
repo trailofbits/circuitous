b9b812bbe93717e521b5f8b7e004305f38e0339d
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-02-14 04:09:12 +0100

    lifter: After flattening only verify function, not entire module.

--- include/circuitous/Lifter/Flatten.hpp
+++ include/circuitous/Lifter/Flatten.hpp
@@ -240,7 +240,6 @@ struct Flattener {
   }
 
   void Run() {
-    auto module = func->getParent();
     auto &context = func->getContext();
 
     auto entry_block = &(func->getEntryBlock());
@@ -256,7 +255,7 @@ struct Flattener {
     for (auto block : orig_blocks) {
       block->eraseFromParent();
     }
-    remill::VerifyModule(module);
+    verify_function(*func);
   }
 }; // struct Flattener
 
