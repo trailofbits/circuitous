adede58358a04b9237035c76a24d5437e9561ae1
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-02 22:34:33 +0200

    run: Extract State class from Spawn.hpp.

--- include/circuitous/Run/Queue.hpp
+++ include/circuitous/Run/Queue.hpp
@@ -56,6 +56,42 @@ namespace circ::run
 
         bool do_enable(Operation *op, uint64_t mem_idx);
         void extend(uint32_t desired);
-
     };
+
+    struct State
+    {
+      private:
+        std::deque< Operation * > todo;
+        std::unordered_map< uint64_t, std::vector< Operation * > > waiting;
+        std::unordered_map< Operation *, uint64_t > blocked;
+
+        MemoryOrdering mem_order;
+
+      public:
+        State(MemoryOrdering mem_order_) : mem_order(std::move(mem_order_)) {}
+        State(const State &) = default;
+        State(State &&) = default;
+
+        State& operator=(State) = delete;
+
+        Operation *pop()
+        {
+            auto x = todo.front();
+            todo.pop_front();
+            return x;
+        }
+
+        void notify_from(Operation *op);
+        void notify(Operation *from, Operation *to);
+        void notify_verbose(Operation *from, Operation *to);
+
+        bool empty() const { return todo.empty(); }
+      private:
+
+        std::string status(Operation *op);
+        void push(Operation *op);
+        void _notify(Operation *op);
+        void notify_mem(Operation *op);
+    };
+
 } // namespace circ::run
