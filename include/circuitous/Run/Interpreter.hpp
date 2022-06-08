06da5ad58a722e57e699f008cc9af8b0d97114ec
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-08 11:31:54 +0200

    run: Thinner Interpreter class.

--- include/circuitous/Run/Interpreter.hpp
+++ include/circuitous/Run/Interpreter.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Trail of Bits, Inc.
+ * Copyright (c) 2020-2022 Trail of Bits, Inc.
  */
 
 #pragma once
@@ -15,110 +15,33 @@ namespace circ::run
     struct QueueInterpreter
     {
         using spawn_t = Spawn;
+        using spawns_t = std::vector< std::unique_ptr< Spawn > >;
+        using spawn_state_t = std::tuple< Memory, NodeState >;
 
         Circuit *circuit;
-
         CtxCollector collector;
-        std::vector< std::pair< VerifyInstruction *, std::unique_ptr< Spawn > > > runners;
 
-        Spawn *acceptor = nullptr;
+        NodeState initial_node_state;
+        Memory initial_memory;
 
-        QueueInterpreter(Circuit *circuit_) : circuit(circuit_)
+        QueueInterpreter(Circuit *circuit,
+                         const NodeState &node_state, const Memory &memory)
+            : circuit(circuit), initial_node_state(node_state), initial_memory(memory)
         {
             collector.Run(circuit);
-
-            for (auto vi : circuit->attr<VerifyInstruction>())
-                runners.emplace_back( vi, std::make_unique< Spawn >(circuit, vi, &collector) );
         }
 
-        void runners_do(auto &&f)
+        spawns_t run_all()
         {
-            for (auto &[_, runner] : runners)
-                f(*runner);
-        }
-
-        void set_input_state(const trace::Entry &in)
-        {
-            return runners_do([ & ]( auto &runner ){ runner.set_input_state(in); } );
-        }
-
-        void set_output_state(const trace::Entry &out)
-        {
-            return runners_do([ & ]( auto &runner ){ runner.set_output_state(out); } );
-        }
-
-        void set_memory(uint64_t addr, const std::string &val)
-        {
-            return runners_do([ & ]( auto &runner ){ runner.set_memory(addr, val); } );
-        }
-
-        template< typename T, typename ...Ts >
-        void init()
-        {
-          for (auto c : circuit->attr< T >())
-              runners_do([ & ]( auto &runner ){ runner.visit(c); } );
-
-          if constexpr (sizeof...(Ts) != 0) {
-              init< Ts... >();
-          }
-        }
-
-        auto values() const
-        {
-            // TODO(lukas): This is dubious once we consider verify mode
-            check(acceptor);
-            return acceptor->node_state.take();
-        }
-
-        void init()
-        {
-            init< Undefined, Constant >();
-        }
-
-        bool result()
-        {
-            uint32_t acceptors = 0;
-            runners_do([ & ](auto &runner ){ if ( *runner.result ) ++acceptors; });
-            return acceptors == 1;
-        }
-
-        std::string dump_spawn(VerifyInstruction *v, Spawn &runner)
-        {
-            std::stringstream ss;
-            ss << runner.GetNodeVal(v)->toString(16, false) << std::endl;;
-            ss << pretty_print(v) << std::endl;
-            for (auto op : v->operands)
+            spawns_t out;
+            for (auto ctx : circuit->attr< VerifyInstruction >())
             {
-                ss << "\t" << op_code_str(op->op_code) << " " << op->id() << " "
-                   << runner.GetNodeVal(op)->toString(16, false) << std::endl;
-            }
-            return ss.str();
-        }
-
-        // TODO(lukas): If runners do is `const` this can be as well.
-        std::string dump_runners()
-        {
-            std::stringstream ss;
-            runners_do([ & ](auto &runner){
-                ss << "TODO(lukas); Implement\n";
-            });
-            return ss.str();
-        }
-
-        bool Run()
-        {
-            init();
-
-            std::unordered_set<Spawn *> successes;
-            for (auto &[_, runner] : runners)
+                auto runner = std::make_unique< Spawn >(
+                        circuit, ctx, &collector, initial_node_state, initial_memory);
                 if (runner->Run())
-                    successes.emplace(runner.get());
-            if (successes.size() == 1)
-                acceptor = *successes.begin();
-
-            check(!(successes.size() > 1)) << "Multiple contexts satisfied."
-                                           << successes.size();
-            return successes.size() == 1;
+                    out.push_back(std::move(runner));
+            }
+            return out;
         }
     };
 
@@ -154,8 +77,6 @@ namespace circ::run
         self_t &set(std::size_t addr, const std::string &val);
         self_t &set(const trace::Entry &trace);
     };
-
-    using interpreter_state_t = std::tuple< Memory, NodeState >;
 
 
 }  // namespace circ::run
