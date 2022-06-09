64c52a7bb004d751b2245898d4389252e456f64f
Sebastiaan Peters <sebastiaan.peters@trailofbits.com>
2022-06-09 16:00:20 +0200

    dec: Generate top-level decode as btree

--- include/circuitous/Decoder/DecoderPrinter.hpp
+++ include/circuitous/Decoder/DecoderPrinter.hpp
@@ -42,6 +42,14 @@ namespace circ::decoder{
       uint8_t msb;
   };
 
+    struct DTI {
+        std::vector< ExtractedVI* > ones;
+        std::vector< ExtractedVI* > zeros;
+        std::vector< ExtractedVI* > ignores;
+    };
+
+
+
   struct innerFunctionArguments {
       innerFunctionArguments(std::array< InputType, 64 > &byte, const std::string &input_name) :byte(byte), input_name(input_name){}
       const std::array< InputType, 64 > &byte;
@@ -95,9 +103,13 @@ namespace circ::decoder{
 
     void print_ignore_bits(const innerFunctionArguments &arg);
 
+      std::string print_split(std::vector<ExtractedVI*> split, std::vector<std::size_t> already_chosen_bits);
+
     std::string get_comparison(const innerFunctionArguments &arg) const;
 
       void print_conversion_input_to_uints64();
+
+      std::string print_evi_call(const ExtractedVI& evi);
   };
 }
 
