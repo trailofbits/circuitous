9b421d8022ad2b37b5dc1d571f6d5c545e6926ba
Sebastiaan Peters <sebastiaan.peters@trailofbits.com>
2022-06-27 13:09:44 +0200

    dec: Refactor OptionalBitArray.

--- lib/Decoder/DecoderPrinter.cpp
+++ lib/Decoder/DecoderPrinter.cpp
@@ -16,25 +16,28 @@ namespace circ::decoder {
     template < int L >
     requires (L <= 64)
     uint64_t OptionalBitArray< L >::to_uint64() const {
-        uint64_t val = 0;
-        for (std::size_t i = 0; i < L; i++) {
-            if ( (*this)[ i ] == InputType::ignore )
-                val |= negate( to_val(InputType::ignore) ) << i;
-            else
-                val |= to_val( (*this)[ i ] ) << i;
-        }
-        return val;
+        auto if_ignore = [&](InputType i){return negate(to_val(InputType::ignore));};
+        auto if_not_ignore = [&](InputType i){return to_val(i);};
+        return convert_to_uint64( if_ignore, if_not_ignore );
     }
 
     template < int L >
     requires (L <= 64)
     uint64_t OptionalBitArray< L >::ignored_bits_to_uint64() const {
+        auto if_ignore = [&](InputType i){return to_val(InputType::ignore);};
+        auto if_not_ignore = [&](InputType i){return negate(to_val(InputType::ignore));};
+        return convert_to_uint64( if_ignore, if_not_ignore );
+    }
+
+    template < int L >
+    requires (L <= 64)
+    uint64_t OptionalBitArray< L >::convert_to_uint64(auto if_ignore, auto if_not_ignore) const {
         uint64_t val = 0;
         for (std::size_t i = 0; i < L; i++) {
             if ( (*this)[ i ] == InputType::ignore )
-                val |= to_val( InputType::ignore ) << i;
+                val |= if_ignore((*this)[ i ]) << i;
             else
-                val |= negate( to_val(InputType::ignore) ) << i;
+                val |= if_not_ignore((*this)[ i ]) << i;
         }
         return val;
     }
