0bfb8ceef44b1c05665e6a18de585653add02e7a
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-07-04 19:25:52 +0200

    run: Use result_t as return type of Interpreter and Spawn run methods.

--- include/circuitous/Run/Interpreter.hpp
+++ include/circuitous/Run/Interpreter.hpp
@@ -24,7 +24,8 @@ namespace circ::run
         using self_t = QueueInterpreter< Spawn >;
 
         using spawn_t = Spawn;
-        using spawns_t = std::vector< std::unique_ptr< Spawn > >;
+        using spawn_ptr_t = std::unique_ptr< Spawn >;
+        using spawns_t = std::vector< spawn_ptr_t >;
         using spawn_state_t = std::tuple< Memory, NodeState >;
 
         Circuit *circuit;
@@ -43,21 +44,27 @@ namespace circ::run
             collector.Run(circuit);
         }
 
+        // Result of the run + the entire spawn for end state investigation.
+        using spawn_result_t = std::tuple< typename Spawn::result_t, spawn_ptr_t >;
+        // Grouped for the whole batch.
+        using result_vector_t = std::vector< spawn_result_t >;
+
         // Returns all the spawns that accepted this run. In case of successful run,
         // there should be one - if there are more there is probably a bug in the lifter
         // or runner.
-        spawns_t run_all()
+        result_vector_t run_all()
         {
-            spawns_t out;
+            result_vector_t results;
+
             for (auto ctx : circuit->attr< VerifyInstruction >())
             {
                 auto runner = std::make_unique< Spawn >(
                         circuit, ctx, &collector, initial_node_state, initial_memory);
                 runner->derive( to_derive );
-                if (runner->run())
-                    out.push_back(std::move(runner));
+                auto status = runner->run();
+                results.push_back(std::make_tuple(status, std::move(runner)));
             }
-            return out;
+            return results;
         }
 
         // Mark some `Operation` types as being able to derive values for some of their
