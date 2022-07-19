60a70b8a26eedb23f24832718328fb9923c2a62f
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-07-19 15:52:47 +0200

    test: Adapt VerifyTest to microx generator api changes.

--- test/full/tools/verify_test.py
+++ test/full/tools/verify_test.py
@@ -20,12 +20,15 @@ class VerifyTest(Test):
             result = True if case.expected.result is None else case.expected.result
             if not self.manual_gen:
                 try:
-                    case.expected = MicroxGen().get(case.input.bytes, case.input, case.input.memory)
+                    trace = MicroxGen().get(case.input.bytes, case.input, case.input.memory)
+                    assert(len(trace) >= 2)
+                    case.input = trace[0]
+                    case.expected = trace[1]
                 except Exception as e:
                     print("Microx fail in: " + self.name + " case " + case.name)
                     print("Bytes: " + case.input.bytes)
                     raise e
-            case.input.mem_hints = case.expected.mem_hints
+            case.expected.mem_hints = case.input.mem_hints
             case.expected = case.expected.mutate(self.e_mutators[idx])
             case.expected.result = result
             case.run_mode = '--verify'
