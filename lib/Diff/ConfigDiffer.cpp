b30211175597654bdb4981a87c64df05cf70f2ee
Sebastiaan Peters <sebastiaan.peters@trailofbits.com>
2022-07-28 19:01:16 +0200

    diff: Templatize diff_subtree extraction function.

--- lib/Diff/ConfigDiffer.cpp
+++ lib/Diff/ConfigDiffer.cpp
@@ -6,43 +6,30 @@
 
 namespace circ::inspect::config_differ{
 
-    void ConfigToTargetDifferPass::Execute(Operation *tree1, Operation* tree2) {
-        /*
-         * We first mark all paths that exist inside tree1 as left.
-         * Then we mark all paths in tree2 as right
-         * If during the walk for tree2 we see left we mark them as overlap
-         */
-        DiffTree( tree1, DiffMarker::Left, DiffMarker::Right );
-        DiffTree( tree2, DiffMarker::Right, DiffMarker::Left );
-    }
-
-    bool is_constraint_tmp(Operation* op ){
-        return isa<constraint_opts_ts>(op);
-    }
-
-    void ConfigToTargetDifferPass::DiffTree(Operation* tree, const DiffMarker& key_this, const DiffMarker& key_other){
-        CTTFinder ctt_collector;
-        ctt_collector.visit( tree );
-
-        std::cout << "found tree ----------"<< std::endl;
-        for(auto& p : ctt_collector.paths_collect){
-            for(auto o : p) {
-                std::cout << o->name() << " " <<  o->id() << " | ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << "----------" << std::endl;
-
-        for(auto& p : ctt_collector.paths_collect){
-            for(auto o : p) {
-                if(o->has_meta(key) && diffmarker_read(o) == key_other)
-                    diffmarker_write(o, DiffMarker::Overlapping);
-                // if something is already marked overlap don't remove it
-                else if(diffmarker_read(o) != DiffMarker::Overlapping)
-                    diffmarker_write(o, key_this);
-            }
-        }
-    }
+//    void ConfigToTargetDifferPass::Execute(Operation *tree1, Operation* tree2) {
+//        /*
+//         * We first mark all paths that exist inside tree1 as left.
+//         * Then we mark all paths in tree2 as right
+//         * If during the walk for tree2 we see left we mark them as overlap
+//         */
+//        DiffTree( tree1, DiffMarker::Left, DiffMarker::Right );
+//        DiffTree( tree2, DiffMarker::Right, DiffMarker::Left );
+//    }
+//
+//    void ConfigToTargetDifferPass::DiffTree(Operation* tree, const DiffMarker& key_this, const DiffMarker& key_other){
+//        CTTFinder ctt_collector;
+//        ctt_collector.visit( tree );
+//
+//        for(auto& p : ctt_collector.paths_collect){
+//            for(auto o : p) {
+//                if(o->has_meta(key) && diffmarker_read(o) == key_other)
+//                    diffmarker_write(o, DiffMarker::Overlapping);
+//                // if something is already marked overlap don't remove it
+//                else if(diffmarker_read(o) != DiffMarker::Overlapping)
+//                    diffmarker_write(o, key_this);
+//            }
+//        }
+//    }
 
     void diffmarker_write(Operation *op, DiffMarker dm) {
         auto value = [&](){
@@ -70,6 +57,14 @@ namespace circ::inspect::config_differ{
             circ::unreachable() << "could not decode DiffMarker";
     }
 
+    void mark_operation(Operation *o, const DiffMarker &key_this, const DiffMarker &key_other) {
+        if(o->has_meta(key) && diffmarker_read(o) == key_other)
+            diffmarker_write(o, DiffMarker::Overlapping);
+            // if something is already marked overlap don't remove it
+        else if(diffmarker_read(o) != DiffMarker::Overlapping)
+            diffmarker_write(o, key_this);
+    }
+
     bool CTTFinder::top(Operation *op) {
         return isa<constraint_opts_ts>(op);
     }
@@ -78,7 +73,11 @@ namespace circ::inspect::config_differ{
         return semantics_tainter::read_semantics(op) == semantics_tainter::SemColoring::Config;
     }
 
-    void CTTFinder::visit(circ::Operation *op) {
-        op->traverse(*this);
+    bool InstrBitsToDR::top(Operation *op) {
+        return isa<DecoderResult>(op);
+    }
+
+    bool InstrBitsToDR::bottom(Operation *op) {
+        return isa<leaf_values_ts>(op);
     }
 }
