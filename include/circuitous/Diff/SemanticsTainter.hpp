7c9b8aa1a6ece93348da84e19e69bf25d7583e55
Sebastiaan Peters <sebastiaan.peters@trailofbits.com>
2022-07-29 20:37:14 +0200

    sem: Change SemanticTainter to be used stand-alone instead of a pass.

--- include/circuitous/Diff/SemanticsTainter.hpp
+++ include/circuitous/Diff/SemanticsTainter.hpp
@@ -78,15 +78,14 @@ namespace circ::inspect::semantics_tainter {
      *
      */
     static const inline std::string key = "diff";
-    struct SemanticsTainterPass : VisitorStartingFromTL<SemanticsTainterPass>, PassBase{
+    struct SemanticsTainterPass : VisitorStartingFromTL<SemanticsTainterPass>{
         void write(Operation* op, SemColoring value);
         bool all_children_are_same(Operation* op);
         bool should_promote_to_semantics(Operation* op);
         void visit(Operation* op);
         void taint(Operation* op);
 
-        CircuitPtr run(CircuitPtr &&circuit) override {start_from<leaf_values_ts>(circuit.get()); return std::move(circuit); }
-        static Pass get() { return std::make_shared< SemanticsTainterPass >(); }
+        void run(const CircuitPtr &circuit) {start_from<leaf_values_ts>(circuit.get()); }
     };
 
 }  // namespace circ
