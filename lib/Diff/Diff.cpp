b36766f070cbd731b9d16afcfaf25ae9f3eb646f
Sebastiaan Peters <sebastiaan.peters@trailofbits.com>
2022-08-02 17:09:20 +0200

    diff: Remove unnecessary else.

--- lib/Diff/Diff.cpp
+++ lib/Diff/Diff.cpp
@@ -6,37 +6,42 @@
 
 namespace circ::inspect
 {
-    void diffmarker_write(Operation *op, DiffMarker dm) {
-        auto value = [&](){
-            switch(dm){
-                case DiffMarker::None : return "none";
-                case DiffMarker::Left : return "left";
-                case DiffMarker::Right : return "right";
-                case DiffMarker::Overlapping : return "overlapping";
+    void diffmarker_write( Operation *op, DiffMarker dm )
+    {
+        auto value = [ & ]()
+        {
+            switch ( dm )
+            {
+                case DiffMarker::None: return "none";
+                case DiffMarker::Left: return "left";
+                case DiffMarker::Right: return "right";
+                case DiffMarker::Overlapping: return "overlapping";
             }
         }();
 
-        op->set_meta<true>( meta_key, value);
+        op->set_meta< true >( meta_key, value );
     }
 
-    DiffMarker diffmarker_read(Operation *op) {
-        if(!op->has_meta( meta_key))
+    DiffMarker diffmarker_read( Operation *op )
+    {
+        if ( !op->has_meta( meta_key ) )
             return DiffMarker::None;
-        else if( op->get_meta( meta_key) == "left")
+        if ( op->get_meta( meta_key ) == "left" )
             return DiffMarker::Left;
-        else if( op->get_meta( meta_key) == "right")
+        if ( op->get_meta( meta_key ) == "right" )
             return DiffMarker::Right;
-        else if( op->get_meta( meta_key) == "overlapping")
+        if ( op->get_meta( meta_key ) == "overlapping" )
             return DiffMarker::Overlapping;
-        else
-            circ::unreachable() << "could not decode DiffMarker";
+
+        circ::unreachable() << "could not decode DiffMarker";
     }
 
-    void mark_operation(Operation *o, const DiffMarker &key_this, const DiffMarker &key_other) {
-        if( o->has_meta( meta_key) && diffmarker_read( o) == key_other)
-            diffmarker_write(o, DiffMarker::Overlapping);
-            // if something is already marked overlap don't remove it
-        else if(diffmarker_read(o) != DiffMarker::Overlapping)
-            diffmarker_write(o, key_this);
+    void mark_operation( Operation *o, const DiffMarker &key_this, const DiffMarker &key_other )
+    {
+        if ( o->has_meta( meta_key ) && diffmarker_read( o ) == key_other )
+            diffmarker_write( o, DiffMarker::Overlapping );
+        // if something is already marked overlap don't remove it
+        else if ( diffmarker_read( o ) != DiffMarker::Overlapping )
+            diffmarker_write( o, key_this );
     }
 }
