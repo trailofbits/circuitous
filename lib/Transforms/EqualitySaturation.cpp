1af5a9f60f9ef6f6974d50c417f7c5b02fd4f8df
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-08-08 10:40:41 +0200

    eqsat: Fix circuit visit patter.

--- lib/Transforms/EqualitySaturation.cpp
+++ lib/Transforms/EqualitySaturation.cpp
@@ -5,21 +5,27 @@
 #include <circuitous/Transforms/EqualitySaturation.hpp>
 #include <circuitous/Transforms/EqSatCost.hpp>
 #include <circuitous/Transforms/EGraph.hpp>
+#include <circuitous/Transforms/EGraphBuilder.hpp>
 
 #include <eqsat/core/egraph.hpp>
 #include <eqsat/algo/saturation.hpp>
 
 namespace circ::eqsat
 {
-    CircuitEGraph make_circuit_egraph(CircuitPtr &&circuit) {
-        return CircuitEGraph{}; // TODO synthesize EGraph
+    EGraphBuilderState make_circuit_egraph(const CircuitPtr &circuit) {
+        EGraphBuilder builder;
+        return builder.build(circuit.get());
     };
 
     CircuitPtr run_equality_saturation(CircuitPtr &&circuit, std::span< RuleSet > rules) {
         log_info() << "Start equality saturation";
-        auto egraph = ::eqsat::saturation(make_circuit_egraph(std::move(circuit)));
-        // TODO run eqsat
-        auto optimal = make_optimal_circuit_graph(std::move(egraph));
+        auto [egraph, nodes_map] = make_circuit_egraph(circuit);
+
+        auto saturation = ::eqsat::saturation(std::move(egraph));
+        // TODO use some algorithm
+        saturation.apply(rules);
+
+        auto optimal = make_optimal_circuit_graph(std::move(saturation));
         log_info() << "Equality saturation stopped";
         return nullptr; // TODO circuit from optimal
     }
