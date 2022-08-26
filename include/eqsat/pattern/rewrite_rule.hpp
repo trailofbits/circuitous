dc27cfd1d020af31e5afa9488507661ed238ffca
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-08-26 22:39:48 +0200

    eqsat: Update rule parsing.

--- include/eqsat/pattern/rewrite_rule.hpp
+++ include/eqsat/pattern/rewrite_rule.hpp
@@ -11,11 +11,24 @@
 
 namespace eqsat {
 
+    static inline match_pattern make_match_pattern(std::string_view pat) {
+        if (auto res = parse_match_pattern(pat))
+            return res.value();
+        throw std::runtime_error("syntax error in match pattern: " + std::string(pat));
+    }
+
+    static inline apply_pattern make_apply_pattern(std::string_view pat) {
+        if (auto res = parse_apply_pattern(pat))
+            return res.value();
+        throw std::runtime_error("syntax error in apply pattern: " + std::string(pat));
+    }
+
+    // TODO implement rewrite rule parser
     struct rewrite_rule {
         rewrite_rule(std::string_view name, std::string_view lhs, std::string_view rhs)
             : name(name)
-            //, lhs(make_pattern(lhs_))
-            //, rhs(make_pattern(rhs_))
+            , lhs(make_match_pattern(lhs))
+            , rhs(make_apply_pattern(rhs))
             //, places(get_indexed_places(lhs))
         {}
 
@@ -23,8 +36,8 @@ namespace eqsat {
 
         // Rewrite rule 'lhs -> rhs' that allows to match
         // left-hand-side and replace it with right-hand-side
-        // Pattern lhs;
-        // Pattern rhs;
+        match_pattern lhs;
+        apply_pattern rhs;
 
         // Places that occur in the rewrite pattern
         // Note: it is required that place occurs on the left hand side
