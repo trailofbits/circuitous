9760878526e7c6e51cea9a3637c64769562000b3
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-09-07 15:51:24 +0200

    lifter:tollvm: Finish implementation of conversion of circIR to LLVM.

--- include/circuitous/Lifter/ToLLVM.hpp
+++ include/circuitous/Lifter/ToLLVM.hpp
@@ -9,6 +9,7 @@
 
 #include <utility>
 #include <memory>
+#include <string>
 
 CIRCUITOUS_RELAX_WARNINGS
 #include <llvm/IR/IRBuilder.h>
@@ -25,8 +26,9 @@ namespace circ
 {
     // Simply convert circuit to llvm function.
     // Returns context and function.
-    auto convert_to_llvm( Circuit *circuit )
-    -> std::tuple< std::shared_ptr< llvm::LLVMContext >, llvm::Function * >;
+    [[ nodiscard ]]
+    std::tuple< std::shared_ptr< llvm::LLVMContext >, std::unique_ptr< llvm::Module > >
+    convert_to_llvm( Circuit *circuit, const std::string &module_name );
 
     // Convert circuit to llvm function using only the provided builder.
     // It is expected this builder already is properly set up - i.e. has an
@@ -34,7 +36,7 @@ namespace circ
     void convert_to_llvm( Circuit *circuit, llvm::IRBuilder<> &irb );
 
     // Convert circuit to llvm function with given name in the provided module.
-    auto convert_to_llvm( Circuit *circuit, llvm::Module *lmodule, const std::string &name )
-    -> llvm::Function *;
+    [[ nodiscard ]] llvm::Function *
+    convert_to_llvm( Circuit *circuit, llvm::Module *lmodule, const std::string &name );
 
 } // namespace circ
