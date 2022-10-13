17f54d893c402b12958f618ee6ff2134a79dcc0a
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-10-13 15:12:01 +0200

    opt: Improve codestyle of Passes.hpp and PassBase.hpp.

--- include/circuitous/Transforms/Passes.hpp
+++ include/circuitous/Transforms/Passes.hpp
@@ -15,66 +15,74 @@
 
 namespace circ
 {
-  namespace detail
-  {
-    inline auto tail(const auto &vec)
+    namespace detail
     {
-      auto tail = std::next(vec.begin());
-      return std::span( &(*tail), vec.size() - 1 );
-    }
-  } // namesapce detail
+        inline auto tail(const auto &vec)
+        {
+          auto tail = std::next(vec.begin());
+          return std::span( &(*tail), vec.size() - 1 );
+        }
+    } // namesapce detail
 
 
-  struct EqualitySaturationPass : PassBase
-  {
-    CircuitPtr run(CircuitPtr &&circuit) override
+    struct EqualitySaturationPass : PassBase
     {
-      return eqsat::EqualitySaturation(std::move(circuit), rulesets);
-    }
+        CircuitPtr run(CircuitPtr &&circuit) override
+        {
+            return eqsat::EqualitySaturation(std::move(circuit), rulesets);
+        }
 
-    static Pass get() { return std::make_shared< EqualitySaturationPass >(); }
+        static Pass get() { return std::make_shared< EqualitySaturationPass >(); }
 
-    void add_rules(std::vector< eqsat::RuleSet > &&ruleset) {
-      rulesets.insert(
-        rulesets.end(),
-        std::make_move_iterator(ruleset.begin()),
-        std::make_move_iterator(ruleset.end())
-      );
-    }
+        void add_rules(std::vector< eqsat::RuleSet > &&ruleset)
+        {
+            rulesets.insert(
+                rulesets.end(),
+                std::make_move_iterator(ruleset.begin()),
+                std::make_move_iterator(ruleset.end())
+            );
+        }
 
-    std::vector<eqsat::RuleSet> rulesets;
-  };
+        std::vector<eqsat::RuleSet> rulesets;
+    };
 
     /*
      * Semantics from remill calculate the overflow flag directly from the values instead of
      * re-using existing flags. This leads to unnecessary computation as we have access
-     * to both input/output carry flags
+     * to both input/output carry flags.
      *
-     * This pass tries to match the generated remill semantics tree completely and patches it out
-     * It does make implicit assumptions on the order of operands
+     * This pass tries to match the generated remill semantics tree completely
+     * and patches it out.
+     * It does make implicit assumptions on the order of operands.
      */
-    bool has_remill_overflow_flag_semantics(RegConstraint* op);
+
+    bool has_remill_overflow_flag_semantics( RegConstraint *op );
 
     struct RemillOFPatch : PassBase
     {
-        CircuitPtr run(CircuitPtr &&circuit) override
+        CircuitPtr run( CircuitPtr &&circuit ) override
         {
-            auto output_of = circuit->fetch_reg<OutputRegister>("OF");
-            auto input_cf = circuit->fetch_reg<InputRegister>("CF");
-            auto output_cf = circuit->fetch_reg<OutputRegister>("CF");
-            if(output_of == nullptr || input_cf == nullptr || output_cf == nullptr)
+            auto output_of = circuit->fetch_reg< OutputRegister >( "OF" );
+            auto output_cf = circuit->fetch_reg< OutputRegister >( "CF" );
+
+            auto input_cf = circuit->fetch_reg< InputRegister >( "CF" );
+
+            if ( !output_of || !input_cf || !output_cf )
                 return std::move(circuit);
 
-            for ( auto regC : circuit->attr< RegConstraint >() )
+            for ( auto reg_constraint : circuit->attr< RegConstraint >() )
             {
-                if ( regC->operands_size() != 2 ||
-                     regC->operand( 1 ) != output_of || !has_remill_overflow_flag_semantics( regC ))
-                continue;
+                if ( reg_constraint->operands_size() != 2 ||
+                     reg_constraint->operand( 1 ) != output_of ||
+                     !has_remill_overflow_flag_semantics( reg_constraint ))
+                {
+                    continue;
+                }
 
-                auto xor_node = circuit.operator->()->create<Xor>(1u);
-                xor_node->add_operand(input_cf);
-                xor_node->add_operand(output_cf);
-                regC->replace_operand(0, xor_node);
+                auto xor_node = circuit->create< Xor >( 1u );
+                xor_node->add_operands( input_cf, output_cf );
+
+                reg_constraint->replace_operand( 0, xor_node );
             }
 
             return std::move(circuit);
@@ -86,13 +94,16 @@ namespace circ
 
     struct MergeAdviceConstraints : PassBase
     {
-        CircuitPtr run(CircuitPtr &&circuit) override
+        CircuitPtr run( CircuitPtr &&circuit ) override
         {
             for ( auto ac : circuit->attr< AdviceConstraint >() )
             {
-                if ( ac->operands_size() != 2 || !isa< Advice >( ac->operand( 0 ) ) ||
-                     !isa< Advice >( ac->operand( 1 ) ))
+                if ( ac->operands_size() != 2 ||
+                     !isa< Advice >( ac->operand( 0 ) ) ||
+                     !isa< Advice >( ac->operand( 1 ) ) )
+                {
                     continue;
+                }
 
                 auto lhs = ac->operand( 0 );
                 auto rhs = ac->operand( 1 );
@@ -112,170 +123,172 @@ namespace circ
     };
 
 
-  struct DummyPass : PassBase
-  {
-    CircuitPtr run(CircuitPtr &&circuit) override { return std::move(circuit); }
-
-    static Pass get() { return std::make_shared< DummyPass >(); }
-  };
-
-  struct TrivialConcatRemovalPass : PassBase
-  {
-      CircuitPtr run( CircuitPtr &&circuit ) override
-      {
-          for ( auto c : circuit->attr< Concat >() )
-              if ( c->operands_size() == 1 )
-                  c->replace_all_uses_with( c->operand( 0 ) );
-
-          return std::move( circuit );
-      }
-
-      static Pass get() { return std::make_shared< TrivialConcatRemovalPass >(); }
-  };
-
-  struct PassesBase
-  {
-      Pass make_known_pass( const std::string &name )
-      {
-            if ( name == "eqsat" )                    return EqualitySaturationPass::get();
-            if ( name == "dummy-pass" )               return DummyPass::get();
-            if ( name == "trivial-concat-removal" )   return TrivialConcatRemovalPass::get();
-            if ( name == "overflow-flag-fix" )        return RemillOFPatch::get();
-            if ( name == "merge-transitive-advices" ) return MergeAdviceConstraints::get();
-            log_kill() << "Unkown pass option:" << name;
-      }
-
-      NamedPass &add_pass( const std::string &name )
-      {
-          auto pass = make_known_pass( name );
-          log_info() << "Adding pass: " << name;
-          return passes.emplace_back( name, pass );
-      }
-
-      CircuitPtr run_pass( const Pass &pass, CircuitPtr &&circuit )
-      {
-          auto result = pass->run( std::move( circuit ) );
-          result->remove_unused();
-          return result;
-      }
-
-      CircuitPtr run_pass( const NamedPass &npass, CircuitPtr &&circuit )
-      {
-          const auto &[ _, pass ] = npass;
-          return run_pass( pass, std::move( circuit ) );
-      }
-
-      std::string report() const { return "no report recorded"; }
-
-      std::vector< NamedPass > passes;
-  };
-
-  template<typename Next>
-  struct WithHistory : Next
-  {
-    using Snapshot = std::pair< std::string, RawNodesCounter >;
-
-    CircuitPtr run_pass(const NamedPass &npass, CircuitPtr &&circuit)
+    struct DummyPass : PassBase
     {
-      const auto &[name, pass] = npass;
+        CircuitPtr run( CircuitPtr &&circuit ) override { return std::move( circuit ); }
 
-      if (history.size() == 0)
-        make_snapshot(circuit);
+        static Pass get() { return std::make_shared< DummyPass >(); }
+    };
 
-      auto result = this->Next::run_pass(npass, std::move(circuit));
-      make_snapshot(result, name);
-      return result;
-    }
-
-    std::string report() const
+    struct TrivialConcatRemovalPass : PassBase
     {
-      using printer = Printer<RawNodesCounter>;
+        CircuitPtr run( CircuitPtr &&circuit ) override
+        {
+            for ( auto c : circuit->attr< Concat >() )
+                if ( c->operands_size() == 1 )
+                {
+                    c->replace_all_uses_with( c->operand( 0 ) );
+                }
 
-      if (history.empty()) {
-        return "empty history";
-      }
+            return std::move( circuit );
+        }
 
-      std::stringstream ss;
+        static Pass get() { return std::make_shared< TrivialConcatRemovalPass >(); }
+    };
 
-      {
-        const auto &[name, def] = history[0];
-        ss << name << ":" << std::endl;
-        printer::Print(ss, def);
-      }
-
-      for (std::size_t i = 1; i < history.size(); ++i) {
-        auto &[name, def] = history[i];
-        ss << name << ":" << std::endl;
-        printer::Diff(ss, history[i - 1].second, def);
-      }
-
-      ss << std::endl << "In the end:" << std::endl;
-      printer::Print(ss, history.back().second);
-      ss << std::endl;
-      return ss.str();
-    }
-
-  private:
-
-    void make_snapshot(const CircuitPtr &circuit, std::optional< std::string > after = std::nullopt)
+    struct PassesBase
     {
-      auto name = after ? after.value() : "start";
-      log_info() << "Start capturing statistics.";
-      RawNodesCounter collector;
-      collector.Run(circuit.get());
-      log_info() << "Done capturing statistics.";
-      history.emplace_back(name, std::move(collector));
-    }
+        Pass make_known_pass( const std::string &name )
+        {
+              if ( name == "eqsat" )                    return EqualitySaturationPass::get();
+              if ( name == "dummy-pass" )               return DummyPass::get();
+              if ( name == "trivial-concat-removal" )   return TrivialConcatRemovalPass::get();
+              if ( name == "overflow-flag-fix" )        return RemillOFPatch::get();
+              if ( name == "merge-transitive-advices" ) return MergeAdviceConstraints::get();
+              log_kill() << "Unkown pass option:" << name;
+        }
 
-    std::vector< Snapshot > history;
-  };
+        NamedPass &add_pass( const std::string &name )
+        {
+            auto pass = make_known_pass( name );
+            log_info() << "Adding pass: " << name;
+            return passes.emplace_back( name, pass );
+        }
 
-  template< typename Next >
-  struct Defensive : Next
-  {
-    CircuitPtr run_pass(const NamedPass &npass, CircuitPtr &&circuit)
+        CircuitPtr run_pass( const Pass &pass, CircuitPtr &&circuit )
+        {
+            auto result = pass->run( std::move( circuit ) );
+            result->remove_unused();
+            return result;
+        }
+
+        CircuitPtr run_pass( const NamedPass &npass, CircuitPtr &&circuit )
+        {
+            const auto &[ _, pass ] = npass;
+            return run_pass( pass, std::move( circuit ) );
+        }
+
+        std::string report() const { return "no report recorded"; }
+
+        std::vector< NamedPass > passes;
+    };
+
+    template< typename Next >
+    struct WithHistory : Next
     {
-      const auto &[name, pass] = npass;
+        using Snapshot = std::pair< std::string, RawNodesCounter >;
 
-      log_info() << "Going to run transformation" << name;
-      auto result = this->Next::run_pass(npass, std::move(circuit));
-      log_info() << "Done. Running verify pass:";
+        CircuitPtr run_pass( const NamedPass &npass, CircuitPtr &&circuit )
+        {
+            const auto &[ name, pass ] = npass;
 
-      auto verify_res = verify_circuit(result.get());
+            if ( history.size() == 0 )
+              make_snapshot(circuit);
 
-      if (verify_res.has_errors())
-        log_kill() << "Verify failed!\n"
-                   << verify_res;
+            auto result = this->Next::run_pass( npass, std::move( circuit ) );
+            make_snapshot( result, name );
+            return result;
+        }
 
-      if (verify_res.has_warnings())
-        log_error() << verify_res.get_warnings();
+        std::string report() const
+        {
+            using printer = Printer< RawNodesCounter >;
 
-      log_info() <<"Circuit is okay.";
-      return result;
-    }
-  };
+            if (history.empty())
+              return "empty history";
 
-  template< typename Next >
-  struct Passes : Next
-  {
-    CircuitPtr run(CircuitPtr &&circuit)
+            std::stringstream ss;
+
+            {
+                const auto &[ name, def ] = history[ 0 ];
+                ss << name << ":" << std::endl;
+                printer::Print( ss, def );
+            }
+
+            for ( std::size_t i = 1; i < history.size(); ++i )
+            {
+                auto &[ name, def ] = history[ i ];
+                ss << name << ":" << std::endl;
+                printer::Diff( ss, history[ i - 1 ].second, def );
+            }
+
+            ss << std::endl << "In the end:" << std::endl;
+            printer::Print( ss, history.back().second );
+            ss << std::endl;
+            return ss.str();
+        }
+
+    private:
+
+        void make_snapshot( const CircuitPtr &circuit,
+                            std::optional< std::string > after = std::nullopt )
+        {
+            auto name = after ? after.value() : "start";
+            log_info() << "Start capturing statistics.";
+            RawNodesCounter collector;
+            collector.Run( circuit.get() );
+            log_info() << "Done capturing statistics.";
+            history.emplace_back( name, std::move( collector ) );
+        }
+
+        std::vector< Snapshot > history;
+    };
+
+    template< typename Next >
+    struct Defensive : Next
     {
-      if (this->passes.empty())
-        return std::move(circuit);
+        CircuitPtr run_pass( const NamedPass &npass, CircuitPtr &&circuit )
+        {
+            const auto &[ name, pass ] = npass;
 
-      CircuitPtr result = run_pass(this->passes.front(), std::move(circuit));
-      for (const auto &pass : detail::tail(this->passes))
-        result = run_pass(pass, std::move(result));
+            log_info() << "Going to run transformation" << name;
+            auto result = this->Next::run_pass( npass, std::move( circuit ) );
+            log_info() << "Done. Running verify pass:";
 
-      return result;
-    }
+            auto verify_res = verify_circuit( result.get() );
 
-    CircuitPtr run_pass(const NamedPass &pass, CircuitPtr &&circuit)
+            if ( verify_res.has_errors() )
+              log_kill() << "Verify failed!\n" << verify_res;
+
+            if ( verify_res.has_warnings() )
+              log_error() << verify_res.get_warnings();
+
+            log_info() <<"Circuit is okay.";
+            return result;
+        }
+    };
+
+    template< typename Next >
+    struct Passes : Next
     {
-      return this->Next::run_pass(pass, std::move(circuit));
-    }
+        CircuitPtr run( CircuitPtr &&circuit )
+        {
+            if ( this->passes.empty() )
+                return std::move( circuit );
 
-    std::string report() const { return this->Next::report(); }
-  };
+            CircuitPtr result = run_pass( this->passes.front(), std::move( circuit ) );
+            for ( const auto &pass : detail::tail( this->passes ) )
+                result = run_pass( pass, std::move( result ) );
+
+            return result;
+        }
+
+        CircuitPtr run_pass( const NamedPass &pass, CircuitPtr &&circuit )
+        {
+          return this->Next::run_pass( pass, std::move( circuit ) );
+        }
+
+        std::string report() const { return this->Next::report(); }
+    };
 
 } // namespace circ
