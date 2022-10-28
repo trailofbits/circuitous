541d306aa299aa7943777500f701c1af3fa9af46
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-10-28 11:57:36 +0200

    test: Allow to use spdlog in tests.

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -6,5 +6,26 @@
  * the LICENSE file found in the root directory of this source tree.
  */
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+#include <spdlog/cfg/env.h>
+#include <spdlog/cfg/argv.h>
+
+
+int main(int argc, char** argv) {
+    spdlog::cfg::load_env_levels();
+    spdlog::cfg::load_argv_levels(argc, argv);
+
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+
+    int res = context.run();
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return res;          // propagate the result of the tests
+
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
+
+    return res + client_stuff_return_code; // the result from doctest is propagated here as well
+}
