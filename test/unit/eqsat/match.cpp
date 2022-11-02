afe1e5967959bb4b29936940a914926b65dfb1e8
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-11-02 14:53:56 +0100

    test: Add ematch tests.

--- test/unit/eqsat/match.cpp
+++ test/unit/eqsat/match.cpp
@@ -9,7 +9,9 @@
 #include <doctest/doctest.h>
 
 #include <eqsat/pattern/rewrite_rule.hpp>
+#include <eqsat/algo/saturation_graph.hpp>
 #include <eqsat/algo/ematch.hpp>
+#include <eqsat/algo/print.hpp>
 
 #include <support/egraph.hpp>
 
@@ -19,8 +21,9 @@ namespace eqsat::test {
     #pragma GCC diagnostic ignored "-Wunused-variable"
     auto count_matches(auto &&matches) {
         std::size_t count = 0;
-        for (auto _ : matches)
+        for (auto _ : matches) {
             count++;
+        }
         return count;
     }
     #pragma GCC diagnostic pop
@@ -58,6 +61,59 @@ namespace eqsat::test {
             CHECK(count_matches(match(rule, egraph)) == 1);
         }
     }
+
+    TEST_CASE("addition") {
+        test_graph egraph;
+
+        auto idx  = make_node(egraph, "x");
+        auto idy  = make_node(egraph, "y");
+        auto add1 = make_node(egraph, "add", idx, idy);
+
+        auto idu  = make_node(egraph, "u");
+        auto idv  = make_node(egraph, "v");
+        auto add2 = make_node(egraph, "add", idu, idv);
+
+        auto rule = rewrite_rule("commutativity", "(op_add ?x ?y)", "(op_add ?y ?x)");
+        CHECK(count_matches(match(rule, egraph)) == 2);
+
+        auto saturable = saturable_egraph(std::move(egraph));
+
+        saturable.merge(add1, add2);
+        saturable.rebuild();
+
+        CHECK(count_matches(match(rule, saturable)) == 2);
+    }
+
+    TEST_CASE("nested additions") {
+        test_graph egraph;
+
+        auto ida  = make_node(egraph, "a:64");
+        auto idb  = make_node(egraph, "b:64");
+        auto idz  = make_node(egraph, "0:64");
+        auto add1 = make_node(egraph, "add", idz, ida);
+        /* auto add2 = */ make_node(egraph, "add", idb, add1);
+
+        auto rule = rewrite_rule("addition", "(op_add ?x (op_add 0:64 ?y))", "(op_add ?x ?y)");
+        CHECK(count_matches(match(rule, egraph)) == 1);
+    }
+
+    TEST_CASE("same arguments") {
+        test_graph egraph;
+        auto ida  = make_node(egraph, "a:64");
+        auto idb  = make_node(egraph, "b:64");
+        /* auto add1 = */ make_node(egraph, "add", ida, idb);
+
+        auto rule = rewrite_rule("twice", "(op_add ?x ?x)", "(op_mul 2:64 ?x)");
+        CHECK(count_matches(match(rule, egraph)) == 0);
+
+        auto saturable = saturable_egraph(std::move(egraph));
+
+        saturable.merge(ida, idb);
+        saturable.rebuild();
+
+        CHECK(count_matches(match(rule, saturable)) == 4);
+    }
+
     } // test suite: eqsat::pattern-patching
 
 } // namespace eqsat::test
