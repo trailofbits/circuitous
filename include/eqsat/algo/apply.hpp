46325d422ab56313e42d67f2b708bf6ce8dbb3bc
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-11-22 15:06:39 +0100

    eqsat: Integrate places into pattern synthesis.

--- include/eqsat/algo/apply.hpp
+++ include/eqsat/algo/apply.hpp
@@ -17,10 +17,11 @@ namespace eqsat {
     node_handle apply(
         const simple_expr &expr,
         const apply_pattern &rule,
+        const places_t &places,
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
-        auto patch = synthesize(expr, rule, where, graph);
+        auto patch = synthesize_simple_expr(expr, rule, places, where, graph);
         return graph.merge(where.root, graph.find(patch));
     }
 
@@ -28,6 +29,7 @@ namespace eqsat {
     node_handle apply(
         const union_expr &un,
         const apply_pattern &rule,
+        const places_t &places,
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
@@ -38,6 +40,7 @@ namespace eqsat {
     node_handle apply(
         const bond_expr &bond,
         const apply_pattern &rule,
+        const places_t &places,
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
@@ -48,21 +51,23 @@ namespace eqsat {
     node_handle apply(
         const apply_action &action,
         const apply_pattern &rule,
+        const places_t &places,
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
         return std::visit([&] (const auto &a) {
-            return apply(a, rule, where, graph);
+            return apply(a, rule, places, where, graph);
         }, action);
     }
 
     template< gap::graph::graph_like egraph >
     node_handle apply(
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
-        return apply(pattern.action, pattern, where, graph);
+        return apply(pattern.action, pattern, places, where, graph);
     }
 
     template< gap::graph::graph_like egraph >
@@ -71,7 +76,8 @@ namespace eqsat {
         const match_result &where,
         saturable_egraph< egraph > &graph
     ) {
-        return apply(rule.rhs, where, graph);
+        auto places = gather_places(rule.lhs);
+        return apply(rule.rhs, places, where, graph);
     }
 
 } // namespace eqsat
