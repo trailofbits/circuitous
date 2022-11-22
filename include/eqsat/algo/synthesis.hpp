25c11dee5b6d4f773dcbd373d9f7ebfa6806579b
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-11-22 15:06:39 +0100

    eqsat: Integrate places into pattern synthesis.

--- include/eqsat/algo/synthesis.hpp
+++ include/eqsat/algo/synthesis.hpp
@@ -15,7 +15,7 @@ namespace eqsat {
         const apply_pattern &pattern,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         return graph.make(constant);
     }
 
@@ -23,9 +23,10 @@ namespace eqsat {
     auto synthesize(
         const operation_t &operation,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         return graph.make(operation);
     }
 
@@ -33,19 +34,23 @@ namespace eqsat {
     auto synthesize(
         const place_t &place,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
-        throw std::runtime_error("not implemented place synthesis");
+    ) -> node_handle {
+        return where.matched_places.find(
+            std::uint32_t(place_index(place, places))
+        )->second.handle();
     }
 
     template< gap::graph::graph_like egraph >
     auto synthesize(
         const label_t &label,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         throw std::runtime_error("not implemented label synthesis");
     }
 
@@ -53,11 +58,12 @@ namespace eqsat {
     auto synthesize(
         const atom_t &atom,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         return std::visit([&] (const auto &a) {
-            return synthesize(a, pattern, where, graph);
+            return synthesize(a, pattern, places, where, graph);
         }, atom);
     }
 
@@ -65,17 +71,18 @@ namespace eqsat {
     auto synthesize(
         const expr_list &list,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         if (is_nested_list(list)) {
             throw std::runtime_error("nested expression in rewrite pattern");
         }
 
-        auto node = synthesize(list.front(), pattern, where, graph);
+        auto node = synthesize_simple_expr(list.front(), pattern, places, where, graph);
 
         for (const auto &child : tail(list)) {
-            synthesize(child, pattern, where, graph);
+            synthesize_simple_expr(child, pattern, places, where, graph);
             // TODO add children to node
         }
 
@@ -83,14 +90,15 @@ namespace eqsat {
     }
 
     template< gap::graph::graph_like egraph >
-    auto synthesize(
+    auto synthesize_simple_expr(
         const simple_expr &expr,
         const apply_pattern &pattern,
+        const places_t &places,
         const match_result &where,
         egraph &graph
-    ) -> typename egraph::node_pointer {
+    ) -> node_handle {
         return std::visit([&] (const auto &e) {
-            return synthesize(e, pattern, where, graph);
+            return synthesize(e, pattern, places, where, graph);
         }, expr);
     }
 
