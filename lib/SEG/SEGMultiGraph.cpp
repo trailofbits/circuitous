77f0ffc4374ac6fe152ea29a5c8b6718fcffb6ea
Sebastiaan Peters <sebastiaan.peters@trailofbits.com:wq>
2022-12-14 13:40:58 +0100

    seg: Remove old and dead code.

--- lib/SEG/SEGMultiGraph.cpp
+++ lib/SEG/SEGMultiGraph.cpp
@@ -12,13 +12,8 @@ namespace circ{
 std::vector< std::shared_ptr<circ::SEGNode> > circ::SEGNode::children()
 {
     return _nodes;
-//    for (auto node : _nodes) {
-//        co_yield node;
-////        for (auto child : node->children()) {
-////            co_yield edge_type{ node, child };
-////        }
-//    }
 }
+
 std::string SEGNode::get_hash() const
 {
     std::stringstream ss;
@@ -69,15 +64,6 @@ void SEGNode::replace_all_nodes_by_id(std::shared_ptr<SEGNode> new_target, std::
 
 }
 
-
-void dfs_graph( const SEGGraph &g )
-{
-    for(auto n : gap::graph::dfs<gap::graph::yield_node::on_close>(g))
-    {
-        std::cout << n->get_hash() << std::endl;
-    }
-
-}
 
 void print_nodes_graph( const SEGGraph &g)
 {
@@ -163,19 +149,16 @@ decoder::FunctionCall print_SEGNode_tree( SEGNode &node, std::string stack_name,
 
 
 /*
- * returns the lvalue of the result of a subtree
+ * This function converts a SEGNode into a c++.
  *
- * will put in all functions it declares inside func_decls
- * HOW TO USE:
- * Give an empty func_decls.
- * It will put all functions and end results as function declartions in func decl
- * The result of this function is primarly used for internal
+ * For a given SEGNode n it will return two things:
+ *      the variable name of an lvalue containing the result of the execution of n together with it's subtrees
+ *      the "setup"/preable code that needs to be executed before the lvalue can be accessed
  *
- * probably needs a global name generator for names?
- * multiple calls inside the same function body without a unique property,
- * SEGNode id's are unique but are all
+ * If a node is costly enough that re-use is warrented we declare the preamble as a function decl
+ *      and add the function to `func_decls`, the lvalue returned will call this newly generated function.
+ *      this function checks whether `func_decls` already include such a function or not.
  */
-
 //TODO(Sebas): change this to a register function and add an api that returns void
 std::pair< decoder::Var, decoder::StatementBlock > expr_for_node(
 //    std::unordered_map<circ::SEGNode, circ::decoder::FunctionDeclaration> &func_decls,
@@ -185,9 +168,6 @@ std::pair< decoder::Var, decoder::StatementBlock > expr_for_node(
     decoder::Var stack )
 {
     std::vector<decoder::Expr> args;
-
-//    args.push_back(pop);
-
     std::vector<decoder::Expr> local_vars;
     decoder::StatementBlock setup;
 
@@ -199,6 +179,7 @@ std::pair< decoder::Var, decoder::StatementBlock > expr_for_node(
         local_vars.push_back(lval);
         setup.push_back(set);
     }
+
     auto pop_call = decoder::FunctionCall(stack.name + ".pop", {});
     auto pop_assign = decoder::Statement(decoder::Assign(pop_var, pop_call));
     setup.push_back(pop_assign);
@@ -228,8 +209,11 @@ std::pair< decoder::Var, decoder::StatementBlock > expr_for_node(
         func_decls.insert( { node, fdb.make() } );
     }
 
+    /*
+     * if we created a function than all code required to create the lvalue will be added
+     * inside the newly created function, and the caller of the function should not duplicate that
+     */
     setup.clear();
-//    setup.push_back(pop_assign);
 
     auto prev_declared_func = func_decls.find( node );
     decoder::VarDecl prev_func_call_var(unique_names_storage.get_unique_var_name());
@@ -237,35 +221,9 @@ std::pair< decoder::Var, decoder::StatementBlock > expr_for_node(
     auto prev_func_assign = decoder::Statement(decoder::Assign(prev_func_call_var, prev_func_call));
     setup.push_back(prev_func_assign);
     return {prev_func_call_var.value(), setup};
-
-
-//    if(node.fd && func_decls.has_function_for_node(node))
-//        return decoder::FunctionCall(func_decls.get_declaration_or_create(node).function_name, {stack});
-//
-//    for(auto &c : node._nodes)
-//    {
-//        args.push_back( expr_for_node( func_decls, *c, stack ) );
-//    }
-
-//    if(node.fd){
-//        decoder::FunctionDeclarationBuilder fdb;
-//        fdb.name("node_" + node.id)
-//            .arg_insert(decoder::VarDecl("stack", "std::stack"))
-//            .retType("void");
-//        fdb.body_insert(decoder::Statement(call_operation(args)));
-//
-//        auto func = fdb.make();
-//        func_decls.func_decls.push_back(func);
-//        return decoder::FunctionCall(func.function_name, { stack } );
-//    }
-//    return {decoder::Var("asd"), decoder::Var("asd")};
-//    return decoder::Statement(call_operation(args));
 }
-decoder::FunctionCall call_operation( std::vector< decoder::Expr > values,
-                                      decoder::Var visitor_name )
-{
-    return decoder::FunctionCall(visitor_name.name, values);
-}
+
+
 
 std::vector< std::shared_ptr<SEGNode> > SEGGraph::nodes() const
 {
