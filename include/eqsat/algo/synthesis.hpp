a3fd98f6020330a681d678406f67240cbc1b2ec7
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-01-06 12:48:30 +0100

    eqsat: Clean up synthesis.

--- include/eqsat/algo/synthesis.hpp
+++ include/eqsat/algo/synthesis.hpp
@@ -13,129 +13,89 @@ namespace eqsat {
     using graph::node_handle;
 
     template< gap::graph::graph_like egraph >
-    auto synthesize_simple_expr(
-        const simple_expr &expr,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle;
+    struct synthesizer {
 
-    template< gap::graph::graph_like egraph >
-    auto synthesize(
-        const expr_with_context &expr,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return synthesize_simple_expr(expr.expr, pattern, places, where, children, graph);
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize_atom(
-        const constant_t &constant,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return graph.insert(constant);
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize_atom(
-        const operation_t &operation,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return graph.insert(operation, children);
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize_atom(
-        const place_t &place,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return where.matched_places.find(
-            std::uint32_t(place_index(place, places))
-        )->second.handle();
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize_atom(
-        const label_t &label,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return synthesize(
-            get_expr_with_name(label, pattern).expr, pattern, places, where, children, graph
-        );
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize(
-        const atom_t &atom,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        return std::visit([&] (const auto &a) {
-            return synthesize_atom(a, pattern, places, where, children, graph);
-        }, atom);
-    }
-
-    template< gap::graph::graph_like egraph >
-    auto synthesize(
-        const expr_list &list,
-        const apply_pattern &pattern,
-        const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
-        egraph &graph
-    ) -> node_handle {
-        if (is_nested_list(list)) {
-            throw std::runtime_error("nested expression in rewrite pattern");
+        auto synthesize(const expr_with_context &e) -> node_handle {
+            return synthesize(e.expr);
         }
 
-        graph::children_t list_children;
-        for (const auto &child : tail(list)) {
-            list_children.push_back(
-                synthesize_simple_expr(child, pattern, places, where, {}, graph)
-            );
+        auto synthesize_atom(const constant_t &constant) -> node_handle {
+            return graph.insert(constant);
+        }
+
+        auto synthesize_operation(const operation_t &operation, std::span< node_handle > children)
+            -> node_handle
+        {
+            return graph.insert(operation, children);
+        }
+
+        auto synthesize_atom(const operation_t &operation) -> node_handle {
+            return synthesize_operation(operation, {});
         }
 
 
-        return synthesize_simple_expr(list.front(), pattern, places, where, list_children, graph);
-    }
+        auto synthesize_atom(const place_t &place) -> node_handle {
+            return match.matched_places.find(
+                std::uint32_t(place_index(place, places))
+            )->second.handle();
+        }
+
+        auto synthesize_atom(const label_t &label) -> node_handle {
+            return synthesize(get_expr_with_name(label, pattern).expr);
+        }
+
+        auto synthesize(const atom_t &atom) -> node_handle {
+            return std::visit([&] (const auto &a) { return synthesize_atom(a); }, atom);
+        }
+
+        auto synthesize(const expr_list &list) -> node_handle {
+            if (is_nested_list(list)) {
+                throw std::runtime_error("nested expression in rewrite pattern");
+            }
+
+            auto front = list.front();
+
+            if (list.size() > 1) {
+                graph::children_t children;
+                for (const auto &child : tail(list)) {
+                    children.push_back(synthesize(child));
+                }
+
+                auto op = std::get< operation_t >(std::get< atom_t >(front));
+                return synthesize_operation(op, children);
+            } else {
+                return synthesize(front);
+            }
+        }
+
+        auto synthesize(const simple_expr &expr) -> node_handle {
+            return std::visit([&] (const auto &e) { return synthesize(e); }, expr);
+        }
+
+        synthesizer(
+              const apply_pattern &pat
+            , const places_t &plc
+            , const single_match_result &m
+            , egraph &g
+        )
+            : pattern(pat), places(plc), match(m), graph(g)
+        {}
+
+        const apply_pattern &pattern;
+        const places_t &places;
+        const single_match_result &match;
+        egraph &graph;
+    };
 
     template< gap::graph::graph_like egraph >
-    auto synthesize_simple_expr(
+    auto synthesize(
         const simple_expr &expr,
         const apply_pattern &pattern,
         const places_t &places,
-        const single_match_result &where,
-        std::span< node_handle > children,
+        const single_match_result &match,
         egraph &graph
     ) -> node_handle {
-        return std::visit([&] (const auto &e) {
-            return synthesize(e, pattern, places, where, children, graph);
-        }, expr);
+        return synthesizer(pattern, places, match, graph).synthesize(expr);
     }
 
 } // namespace eqsat
