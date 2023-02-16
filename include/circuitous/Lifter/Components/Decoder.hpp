a44c0d834bfd0ea5efdcf63ec628b65262b07631
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-16 18:18:43 +0100

    lifter:decoder: Implement decoder for Atom class.

--- include/circuitous/Lifter/Components/Decoder.hpp
+++ include/circuitous/Lifter/Components/Decoder.hpp
@@ -20,34 +20,67 @@
 
 namespace circ::build
 {
-    struct Decoder
+    struct DecoderBase
     {
-        using values_t = std::vector< llvm::Value * >;
-
-        llvm::IRBuilder<> &ir;
-
         using shadow_t = typename InstructionInfo::shadow_t;
         using shadows_t = std::vector< shadow_t >;
+
+        using values_t = std::vector< llvm::Value * >;
         using enc_t = typename InstructionInfo::enc_t;
 
-        using navig_t = shadowinst::navig;
+        llvm::IRBuilder<> &irb;
+
+        DecoderBase( llvm::IRBuilder<> &irb ) : irb( irb ) {}
+
+        static std::string generate_raw_bytes( const std::string &str,
+                                               uint64_t from, uint64_t to );
+
+        static std::string convert_encoding( const auto &encoding )
+        {
+            std::string full_inst;
+            // Encoding check needed since `x` is unsigned.
+            for ( std::size_t i = 0; i < encoding.size(); ++i )
+                full_inst += ( encoding[ i ] ) ? '1' : '0';
+
+            return full_inst;
+        }
+
+
+        llvm::Value *create_bit_check( const shadow_t &shadow,
+                                       uint64_t from, uint64_t to);
+
+        values_t byte_fragments( const shadow_t &shadow );
+
+        llvm::Value *emit_translation_tree( const shadowinst::Reg &sreg,
+                                            llvm::Value *selector );
+
+        llvm::Value *holes( const shadow_t &shadow );
+        llvm::Value *holes( const shadowinst::Reg &sreg );
+
+        llvm::Value *hardcoded_check( const shadow_t &shadow );
+
+    };
+
+    struct Decoder : protected DecoderBase
+    {
+        using base_t = DecoderBase;
 
         const shadows_t &shadows;
 
         std::vector< llvm::Value * > to_verify;
 
-        Decoder( llvm::IRBuilder<> &ir, ISEL_view &isel )
-            : ir( ir ),
+        Decoder( llvm::IRBuilder<> &irb, ISEL_view &isel )
+            : base_t( irb ),
               shadows( isel.shadows )
         {}
 
-        Decoder( llvm::IRBuilder<> &ir, const InstructionInfo &info )
-            : ir( ir ),
+        Decoder( llvm::IRBuilder<> &irb, const InstructionInfo &info )
+            : base_t( irb ),
               shadows( info.shadows )
         {}
 
-       Decoder( llvm::IRBuilder<> &ir, const shadows_t &shadows )
-            : ir( ir ),
+       Decoder( llvm::IRBuilder<> &irb, const shadows_t &shadows )
+            : base_t( irb ),
               shadows( shadows )
         {}
 
@@ -55,25 +88,21 @@ namespace circ::build
         auto hardcoded_checks() -> values_t;
       private:
 
-        values_t byte_fragments( const shadow_t &shadow );
-        std::string generate_raw_bytes(const std::string &str, uint64_t form, uint64_t to);
-        llvm::Value *create_bit_check( const shadow_t &shadow,
-                                       uint64_t from, uint64_t to);
+        llvm::Value *all_holes();
+    };
 
-        std::string convert_encoding(const auto &encoding)
-        {
-            std::string full_inst;
-            // Encoding check needed since `x` is unsigned.
-            for (std::size_t i = 0; i < encoding.size(); ++i)
-                full_inst += (encoding[i]) ? '1' : '0';
+    struct AtomDecoder : protected DecoderBase
+    {
+        using base_t = DecoderBase;
+        using atom_t = Atom;
 
-            return full_inst;
-        }
+        const atom_t &atom;
 
-        llvm::Value *emit_translation_tree( const shadowinst::Reg &sreg,
-                                            llvm::Value *selector );
+        AtomDecoder( llvm::IRBuilder<> &irb, const atom_t &atom )
+            : base_t( irb ), atom( atom )
+        {}
 
-        std::tuple< values_t, values_t > tie_selectors( const values_t &conds );
+        llvm::Value *get_decoder_tree();
     };
 
 }  // namespace circ::build
