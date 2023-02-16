7a604045e546b18b4f1e1c2c6b48e89d6c6fd821
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-16 21:31:57 +0100

    treewide: Add verbose flag to circuit stat reporter.

--- include/circuitous/Transforms/Passes.hpp
+++ include/circuitous/Transforms/Passes.hpp
@@ -230,7 +230,7 @@ namespace circ
             {
                 const auto &[ name, def ] = history[ 0 ];
                 ss << name << ":" << std::endl;
-                printer::Print( ss, def );
+                printer::Print( ss, def, false );
             }
 
             for ( std::size_t i = 1; i < history.size(); ++i )
@@ -241,7 +241,7 @@ namespace circ
             }
 
             ss << std::endl << "In the end:" << std::endl;
-            printer::Print( ss, history.back().second );
+            printer::Print( ss, history.back().second, false );
             ss << std::endl;
             return ss.str();
         }
