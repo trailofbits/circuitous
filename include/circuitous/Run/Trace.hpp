f04666e43bcc1064cf0cf11d1cbe85456a4a2bd3
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-07-26 16:54:25 +0200

    run:trace:mttn: Add support to all relevant field types.

--- include/circuitous/Run/Trace.hpp
+++ include/circuitous/Run/Trace.hpp
@@ -43,12 +43,26 @@ namespace circ::run::trace
         }
     };
 
-    llvm::APInt cast( const std::string str, std::size_t radix )
+    llvm::APInt cast( const std::string str, std::size_t radix, std::size_t size )
     {
-        auto value = MaybeValue( str ).cast( str.size(), radix );
+        auto value = MaybeValue( str ).cast( size, radix );
         check( value ) << "Could not cast from \"" << str << "\" in radix" << radix
                        <<  "to APInt:" << str;
         return *value;
+    }
+    llvm::APInt cast( const std::string str, std::size_t radix )
+    {
+        return cast( str, radix, str.size() );
+    }
+
+    llvm::APInt construct_inst_bits( const std::string &str, std::size_t size, uint8_t radix )
+    {
+        std::string reordered;
+
+        check( str.size() >= 2 ) << str.size() << " content:" << str;
+        for (int i = static_cast< int >( str.size() - 2 ); i >=0; i -=2 )
+            reordered += str.substr( static_cast< unsigned long >( i ), 2 );
+        return llvm::APInt( static_cast< uint32_t >( size ), reordered, radix );
     }
 
     namespace native
@@ -230,17 +244,6 @@ namespace circ::run::trace
                     return *this;
                 }
 
-                llvm::APInt construct_inst_bits(
-                        const std::string &str, std::size_t size, uint8_t radix)
-                {
-                    std::string reordered;
-
-                    check(str.size() >= 2) << str.size() << " content:" << str;
-                    for (int i = static_cast< int >(str.size() - 2); i >=0; i -=2)
-                        reordered += str.substr(static_cast< unsigned long >(i), 2);
-                    return llvm::APInt(static_cast< uint32_t >(size), reordered, radix);
-                }
-
                 Trace::Entry take() { return std::move(entry); }
             };
         };
@@ -299,9 +302,17 @@ namespace circ::run::trace
 
             auto next( std::size_t size ) -> value_type
             {
-                current += size;
+                log_dbg() << "[run::trace::mttn]:" << "[ "<< current / 8 << ","
+                                                   << ( current + size ) / 8 << ")";
                 check( !eof() );
+                current += size;
                 return storage.substr( current - size, size );
+            }
+
+            template< typename ... Args > requires ( sizeof ... ( Args ) > 1 )
+            auto next( Args ... args )
+            {
+                return std::make_tuple( next( static_cast< std::size_t >( args ) ) ... );
             }
 
             static lexer from_string(const std::string &data)
@@ -316,8 +327,7 @@ namespace circ::run::trace
             {
                 "EAX", "EBX", "ECX", "EDX",
                 "ESI", "EDI",
-                "ESP", "EBP",
-                "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"
+                "ESP", "EBP"
             };
 
             inline static const std::vector< std::string > syscall_regs =
@@ -339,9 +349,10 @@ namespace circ::run::trace
                 static constexpr inline const std::size_t maximum = 2;
                 // Operation mask, data + Addr (big endian) + Value (big endian)
                 const std::size_t size = 8 + 32 + 32;
+
             };
 
-            struct syscall_reg : protected reg
+            struct syscall_reg : public reg
             {
                 using reg::reg;
             };
@@ -361,7 +372,10 @@ namespace circ::run::trace
 
             static gap::generator< entry > fields()
             {
-                co_yield entry{ inst_bytes{} };
+                // Trace is actually written in a reverse order
+
+                for ( std::size_t i = 0; i < memory_hint::maximum; ++i )
+                    co_yield entry{ memory_hint{} };
 
                 for ( auto reg_name : gpr )
                     co_yield entry{ reg( reg_name ) };
@@ -369,115 +383,207 @@ namespace circ::run::trace
                 for ( auto reg_name : syscall_regs )
                     co_yield entry{ syscall_reg( reg_name ) };
 
-                co_yield entry{ reg{ "EIP" } };
+                co_yield entry{ reg( "EIP" ) };
+
                 co_yield entry{ eflags{} };
 
-                for ( std::size_t i = 0; i < memory_hint::maximum; ++i )
-                    co_yield entry{ memory_hint{} };
+                co_yield entry{ inst_bytes{} };
             }
         };
 
-        template< typename L >
+        template< typename L, typename D >
         struct parser : trace_description
         {
-            using self_t = parser< L >;
+            using self_t = parser< L, D >;
 
             using lexer_type = L;
-            using parse_map = std::map< std::string, typename lexer_type::value_type >;
+            using decoder_type = D;
+            using parse_map = std::map< std::string, std::optional< llvm::APInt > >;
 
             using entry = typename trace_description::entry;
           protected:
 
             const std::string &data;
             lexer_type lexer;
+            std::size_t ts;
+            decoder_type decoder;
 
             parse_map parsed;
 
-            parser( const std::string &data )
-                : data( data ), lexer( L::from_string( data ) )
+            std::size_t memory_hint_idx = 0;
+
+            parser( const std::string &data, std::size_t ts, D decoder )
+                : data( data ), lexer( L::from_string( data ) ), ts( ts ), decoder( decoder )
             {}
 
-            void handle( auto && ) { log_kill() << "Not implemented."; }
+            void assign( const std::string &name, const std::string &value )
+            {
+                return assign( name, cast( value, 2 ) );
+            }
+
+            void assign( const std::string &name, llvm::APInt value )
+            {
+                log_dbg() << "[run::trace::mttn]:" << name << ":="
+                          << llvm::toString( value, 2, false );
+                check( !parsed.count( name ) ) << name;
+                parsed[ name ] = { value };
+            }
+
+            void handle( auto ) { log_kill() << "Not implemented."; }
 
             void handle( trace_description::reg reg )
             {
                 auto value = lexer.next( reg.size );
-                check( !parsed.count( reg.name ) );
-                parsed[ reg.name ] = value;
+                assign( reg.name, value );
             }
 
             void handle( trace_description::memory_hint hint )
             {
+                auto [ head, addr, value ] = lexer.next( 8, 32, 32 );
 
+                // We are only parsing `head`, so I am baking it in.
+                auto at = [&, head = head ]( std::size_t idx )
+                {
+                    return head.substr( head.size() - 1 - idx, 1);
+                };
+
+
+                auto used = at( 7 );
+                auto read = at( 2 );
+                // Pad to 4 bits as we expect.
+                auto size = "00" + at( 1 ) + at( 0 );
+
+                std::vector< llvm::APInt > partials = {
+                    cast( used, 2 ),
+                    // llvm::APInt( 1, 1, false ),
+                    cast( read, 2 ),
+                    // ( *convert( o.getString( "mode" ), 64 ) ).trunc( 1 ),
+                    llvm::APInt( 6, 0, false ), // reserved
+                    llvm::APInt( 4, memory_hint_idx, false ), // id
+                    // ( *convert( o.getString( "id" ), 64 ) ).trunc( 4 ),
+                    cast( size, 2 ),
+                    //( *convert( o.getString( "size" ), 64 ) ).trunc( 4 ),
+                    cast( addr, 2 ),
+                    // *convert( o.getString( "addr" ), 64 ),
+                    cast( value, 2 ),
+                    // *convert( o.getString( "val" ), 64 ),
+                    llvm::APInt( 64, ts, false ) // ts
+                    // *convert( o.getString( "ts" ), 64 )
+                };
+
+                // We know mttn can only produce 32bit traces.
+                irops::memory::Parsed< llvm::APInt > reconstructed( 32, std::move( partials ) );
+                llvm::APInt out { irops::memory::size( 32 ), 0, false };
+                auto inserter = [ & ]( auto thing, auto from, auto size )
+                {
+                    check( size == thing.getBitWidth() );
+                    out.insertBits( thing, from );
+                };
+
+                irops::memory::construct( reconstructed, inserter );
+                assign( "memory." + std::to_string( memory_hint_idx++ ), out );
+            }
+
+            void handle( trace_description::inst_bytes inst )
+            {
+                auto token = lexer.next( inst.size );
+                auto actual_size = decoder( token ) * 8;
+
+                auto enc = token.substr( token.size() - actual_size, actual_size );
+                auto padded = std::string( token.size() - actual_size, '0' ) + enc;
+
+                assign( "instruction_bits", cast( padded, 2, 15 * 8 ) );
             }
 
             void handle( trace_description::eflags eflags )
             {
                 auto token = lexer.next( eflags.size );
-                auto at = [&]( auto idx ) { return token[ eflags.size - idx ]; };
+                auto at = [&]( auto idx )
+                {
+                    return token.substr( eflags.size - 1 - idx, 1 );
+                };
+
                 // 0 CF
-                parsed[ "CF" ] = at( 0ul );
+                assign( "CF", at( 0ul ) );
                 // 2 PF
-                parsed[ "PF" ] = at( 2ul );
+                assign( "PF", at( 2ul ) );
                 // 4 AF
-                parsed[ "AF" ] = at( 4ul );
+                assign( "AF", at( 4ul ) );
                 // 6 ZF
-                parsed[ "ZF" ] = at( 6ul );
+                assign( "ZF", at( 6ul ) );
                 // 7 SF
-                parsed[ "SF" ] = at( 7ul );
+                assign( "SF", at( 7ul ) );
+                // 9 SF
+                assign( "DF", at( 7ul ) );
                 // 11 OF
-                parsed[ "DF" ] = at( 11ul );
-                check( at( 1ul ) == '1' && at( 3ul ) == '0' && at( 5ul ) == '0' )
+                assign( "OF", at( 11ul ) );
+
+                // Fixed values by eflags spec.
+                check( at( 1ul ) == "1" && at( 3ul ) == "0" && at( 5ul ) == "0" )
                     << "Sanity check of eflags encoding failed!";
 
             }
 
-            void handle( trace_description::syscall_reg )
+            void handle( trace_description::syscall_reg syscall_reg )
             {
+                std::ignore = lexer.next( syscall_reg.size );
                 log_dbg() << "[run::trace::mttn]:" << "Ignoring syscall_regs for now!";
                 return;
             }
 
+            void add_missing_fields()
+            {
+                assign( "timestamp", llvm::APInt( 64, ts, false ) );
+                assign( "error_flag", llvm::APInt( 1, 0, false ) );
+
+                assign( "SSBASE", llvm::APInt( 64, 0, false ) );
+                assign( "DSBASE", llvm::APInt( 64, 0, false ) );
+                assign( "FSBASE", llvm::APInt( 64, 0, false ) );
+                assign( "CSBASE", llvm::APInt( 64, 0, false ) );
+                assign( "GSBASE", llvm::APInt( 64, 0, false ) );
+                assign( "ESBASE", llvm::APInt( 64, 0, false ) );
+            }
+
             auto parse() &&
             {
+                std::stringstream ss;
+                ss << "Loading trace words:\n";
+                for ( std::size_t i = 0; i < lexer.storage.size(); i += 8 )
+                    ss << "\t" << i / 8 << ": " << lexer.storage.substr( i, 8 );
+                log_dbg() << "[run::trace::mttn]:" << ss.str();
+
                 auto dispatch = [&](auto field) { return this->handle( field ); };
 
                 for ( auto e : trace_description::fields() )
                     std::visit( dispatch, e );
+
+
+                // We know some fields are simply not present in mttn
+                add_missing_fields();
 
                 return std::move( parsed );
             }
 
           public:
 
-            static parse_map parse( const std::string &data )
+            static parse_map parse( const std::string &data, std::size_t ts, auto decoder )
             {
-                return parser< lexer_type >( data ).parse();
-            }
-
-            static auto as_native_entry( const std::string &data ) -> native::Trace::Entry
-            {
-                auto parse_map = parse( data );
-
-                native::Trace::Entry out;
-                for ( auto [ key, val ] : parse_map )
-                    out[ key ] = cast( val, 2 );
-
-                return out;
+                return parser< lexer_type, decltype( decoder ) >( data, ts, decoder ).parse();
             }
         };
 
-
-        auto load( const std::string &src, const ::circ::Trace &trace_fmt )
+        auto load( const std::string &src, const ::circ::Trace &trace_fmt, auto decoder )
             -> native::Trace
         {
             std::ifstream input(src);
             check( input ) << "Problem opening file to load mttn trace from:" << src;
 
             native::Trace out;
+            std::size_t idx = 0;
             for ( std::string line; std::getline( input, line ); )
             {
-                auto entry = parser< lexer >::as_native_entry( line );
+                auto entry = parser< lexer, decltype( decoder ) >::parse( line,
+                                                                          idx++, decoder );
                 out.entries.emplace_back( std::move( entry ) );
             }
 
