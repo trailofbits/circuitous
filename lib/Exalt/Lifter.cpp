f1ed7798418e513d9d1e8aa668031f657a3a3f37
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-09-05 19:54:43 +0200

    exalt: Add more skeleton interfaces of components.

---
+++ lib/Exalt/Lifter.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Trail of Bits, Inc.
+ */
+
+#include <circuitous/Exalt/Lifter.hpp>
+
+#include <circuitous/Support/Log.hpp>
+#include <circuitous/Util/Warnings.hpp>
+#include <circuitous/Support/Check.hpp>
+
+CIRCUITOUS_RELAX_WARNINGS
+CIRCUITOUS_UNRELAX_WARNINGS
+
+namespace circ
+{
+    void unit_lifter::exalt( unit_t &unit )
+    {
+        std::ignore = unit;
+    }
+
+    void circuit_producer::exalt( unit_t &unit )
+    {
+        std::ignore = unit;
+    }
+
+    void circuit_producer::finalize()
+    {
+
+    }
+
+}  // namespace circ
