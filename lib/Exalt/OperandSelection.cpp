d98b0f6c8c1478e0637002c6b61271e898e88139
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-10-04 14:54:46 +0200

    exalt: Finish basic implementation of `operand_selection`.

--- lib/Exalt/OperandSelection.cpp
+++ lib/Exalt/OperandSelection.cpp
@@ -7,9 +7,59 @@
 CIRCUITOUS_RELAX_WARNINGS
 CIRCUITOUS_UNRELAX_WARNINGS
 
-
 namespace circ::exalt
 {
+    value_t make_mux( builder_t &irb, State &state, const shadowinst::TM_t &tm )
+    {
+        values_t args( ( 1 << tm.bitsize ) + 1, nullptr );
+        args[ 0 ] = irb.getIntN( static_cast< unsigned >( tm.bitsize ), 0 );
+
+        auto &ctx = state.ctx;
+        for ( const auto &[ str, reg_name ] : tm.reverse_bitmap() )
+        {
+            if ( reg_name.starts_with( "__remill_zero" ) )
+                continue;
+
+            auto idx = llvm::APInt( static_cast< uint32_t >( tm.bitsize ), str, 2 )
+                .getLimitedValue();
+
+            check( args.size() > idx + 1 );
+
+            // Pseudo regs *do not have* remill register objects.
+            auto value = [ & ]()
+            {
+                auto parent_reg = enclosing_reg( ctx.arch(), reg_name );
+                if ( parent_reg )
+                    return state.load( irb, parent_reg );
+                return state.load( irb, reg_name );
+            }();
+
+            auto coerced = [ & ]
+            {
+                if ( ctx.bw( value ) < ctx.ptr_size )
+                    return irb.CreateZExt( value, ctx.word_type() );
+                return value;
+            }();
+
+            args[ idx + 1 ] = coerced;
+        }
+
+        auto trg_type = [ & ]()
+        {
+            for ( std::size_t i = 1; i < args.size(); ++i)
+                if ( args[ i ] )
+                    return args[ i ]->getType();
+            log_kill() << "Trying to create select without values";
+        }();
+
+        auto zero = llvm::ConstantInt::get( trg_type, 0, false );
+        for ( std::size_t i = 0; i < args.size(); ++i )
+            if ( !args[ i ] )
+                args[ i ] = zero;
+
+        return irops::Select::make( irb, args );
+    }
+
     auto TM_cache::upcast( const translation_map_t &tm )
         -> translation_map_t
     {
@@ -75,25 +125,29 @@ namespace circ::exalt
     std::string TM_cache::to_string() const
     {
         std::stringstream ss;
-        ss << "TM_cache:\n";
+        ss << "TM_cache with " << storage.size() << " entries:\n";
         for ( const auto &tm : storage )
             ss << tm.to_string( 1 ) << "\n";
         return ss.str();
     }
 
-    operand_selector::operand_selector( const shadowinst::TM_t &tm )
-    {
+    operand_selector::operand_selector( builder_t &irb, State &state,
+                                        const shadowinst::TM_t &tm )
+        : mux( make_mux( irb, state, tm ) )
+    {}
 
+    void operand_selector::add_user( value_t condition, value_t value )
+    {
+        selectors[ value ].emplace( condition );
     }
 
-    void operand_selector::add_user( value_t condition, value_t selector )
+    value_t operand_selector::update_mux( builder_t &irb )
     {
-
-    }
-
-    value_t operand_selector::build_mux()
-    {
-        return nullptr;
+        auto selector = irops::Switch::make< irops::Option >( irb, selectors );
+        auto as_inst = llvm::dyn_cast< llvm::Instruction >( mux );
+        check( as_inst );
+        as_inst->setOperand( 0, selector );
+        return mux;
     }
 
 } // namespace circ::exalt
