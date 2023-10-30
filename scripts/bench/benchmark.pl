cadadb8446f693d24199aecda40a6f770e16f767
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-10-30 10:38:42 +0100

    bench: Descibe benchmark options.

--- scripts/bench/benchmark.pl
+++ scripts/bench/benchmark.pl
@@ -15,6 +15,7 @@ my $output_dir = "results";
 my $runner = "docker";
 my $container = "circuitous:latest";
 my $nocache;
+my $tabulate;
 my $verbose;
 my $help;
 
@@ -26,6 +27,7 @@ GetOptions(
     'container=s'   => \$container,
     'verbose'       => \$verbose,
     'no-cache'      => \$nocache,
+    'tabulate'      => \$tabulate,
     'help'          => \$help,
 ) or pod2usage(2);
 
@@ -43,14 +45,32 @@ benchmark.pl - A script to build and execute circuitous benchmarks in a containe
 
 benchmark.pl [options]
 
-Options:
+Runner options:
 
   --output <folder>     Output file for results
   --runner <string>     Runner (Default: "docker")
   --container <string>  Container name (Default: "circuitous:latest")
   --no-cache            To build container withour cache.
+  --tabulate            Print results table on-the-fly.
   --verbose             Enable verbose mode
   --help                Display this help message
+
+Forwarded benchmark options:
+
+  --benchmark_list_tests={true|false}
+  --benchmark_filter=<regex>
+  --benchmark_min_time=`<integer>x` OR `<float>s`
+  --benchmark_min_warmup_time=<min_warmup_time>
+  --benchmark_repetitions=<num_repetitions>
+  --benchmark_enable_random_interleaving={true|false}
+  --benchmark_report_aggregates_only={true|false}
+  --benchmark_display_aggregates_only={true|false}
+  --benchmark_format=<console|json|csv>
+  --benchmark_out=<filename>
+  --benchmark_out_format=<json|console|csv>
+  --benchmark_color={auto|true|false}
+  --benchmark_context=<key>=<value>,...
+  --benchmark_time_unit={ns|us|ms|s}
 
 =cut
 
@@ -64,7 +84,6 @@ sub build {
 
     if ($verbose && $? == 0) {
         say "Docker built successfully.";
-        say "Output:\n$output";
     } else {
         say "Error building docker: $?";
     }
@@ -75,6 +94,8 @@ sub run {
     if ($verbose) {
         say "Running: $cmd";
     }
+
+    my $output = `$cmd`;
 }
 
 sub main {
@@ -89,6 +110,10 @@ sub main {
     build($build_cmd);
 
     my $run_cmd = "$runner run -v $output_dir:/tmp/output -dt $container" . " " . join(" ", @unparsed_options);
+
+    if ($tabulate) {
+        $run_cmd .= "--benchmark_counters_tabular=true"
+    }
 
     run($run_cmd);
 }
