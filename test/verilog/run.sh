1553ddd72f5ffb9278bd3cf17986ffb78f6d788c
xlauko <xlauko@mail.muni.cz>
2024-02-23 13:37:30 +0000

    test: Refactor runner.

--- test/verilog/run.sh
+++ test/verilog/run.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
 
-failed=0
+circuit=""
 
-for trace in input_mttn_traces/*; do
-    sh scripts/from-trace.sh $trace >&2 || failed=1
+process_traces() {
+    local script="$1"
+    local failed=0
+    for trace in input_mttn_traces/*; do
+        sh "$script" "$trace" >&2 || failed=1
+    done
+    return $failed
+}
+
+display_help() {
+    echo "Usage: $0 [--circuit <circuit_file>] [--help]"
+    echo "Options:"
+    echo "  --circuit <circuit_file>    Specify the circuit for execution"
+    echo "  --help                      Display this help message"
+    exit 0
+}
+
+# Process command-line options
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --circuit)
+            circuit="$2"
+            shift 2
+            ;;
+        --help)
+            display_help
+            ;;
+        *)
+            shift
+            ;;
+    esac
 done
 
-if [ $failed -ne 0 ]; then
-    exit 1
+# Generate circuit from trace when --circuit was not provided
+if [ -z "$circuit" ]; then
+    process_traces scripts/from-trace.sh
+else
+    process_traces "scripts/ciff-and-mttn-trace.sh $circuit"
 fi
 
-#tiny_86="inputs/tiny86.ciff"
-#for trace in input_mttn_traces/*; do
-#    sh scripts/ciff-and-mttn-trace.sh $tiny_86 $trace >&2 || failed=1
-#done
-
-
-if [ $failed -ne 0 ]; then
-    exit 1
-fi
-
-exit $failed
+exit $?
